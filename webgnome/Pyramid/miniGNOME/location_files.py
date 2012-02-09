155bd0e8d6e7c31e61cd18b79a9117e23598b31f
Chris <Chris.Barker@noaa.gov>
2012-02-09 11:41:26 -0800

    added a new module with classes for Location files

---
+++ webgnome/Pyramid/miniGNOME/location_files.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""
+Classes for Location files
+
+These shoudl hold all the information about how GNOME is set up for a given location
+
+"""
+
+class LocationFile:
+    """
+    The base class for a Location File
+    
+    There isn't much here, but it will serve as documentation for the interface
+   """
+
+
