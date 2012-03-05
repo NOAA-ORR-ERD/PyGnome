191b601a92ca0d0413444e634985d1d7a04d4d83
Brian_Zelenke <brian.zelenke@noaa.gov>
2012-03-05 11:02:56 -0800

    Added test to compare coordinates of a particle moved over multiple time-steps vs. the coordinates returned when moving a particle the same distance/direction in a single time-step.

--- py_gnome/tests/test_WindMover.py
+++ py_gnome/tests/test_WindMover.py
@@ -278,3 +278,85 @@ def test_northeastward():
         print "Error:  pyGNOME latitude and longitude (%g, %g) are NOT equivalent to analytical latitude and longitude (%g, %g)." %(EndLat,EndLon,knownLat,knownLon)
     
     assert(np.allclose(EndLat,knownLat) and np.allclose(EndLon,knownLon)) #If the analytic and pyGNOME results differ too much, thow an error so the test fails.
+
+
+def test_northward_stepwise():
+    '''
+    Use the Python implementation of GNOME to test moving a particle, forced
+    only by wind, to the north over multiple time-steps.  Ensure that the end
+    coordinate returned when subdividing a run into multiple time-steps is the
+    same as calculated when moving the particle the same distance/direction in
+    a single time-step.
+    '''
+    
+    #Define input parameters.
+    num_particles=1.0 #Number of LEs
+    LatIn=26.0 #Starting latitude in decimal degrees.
+    LonIn=-88.0 #Starting longitude in decimal degrees.
+    WindPrct=1.0 #Windage in percentage (as a decimal).
+    WindU=0.0 #U component of wind velocity in m/s.
+    WindV=1.0 #V component of wind velocity in m/s.
+    TotalT=1000.0 #Total run duration, in seconds.
+    Tdivisor=10.0 #Number to divide TotalT by into time-steps (typically an integer evenly divides TotalT).
+    
+    #Fill-in array with the input parameters.
+    npra = np.zeros(num_particles, dtype=le_rec) #Initialize and empty array of zeros.
+    npra["p"]=(LonIn,LatIn) #World point longitude,latitude.
+    npra["windage"]=(WindPrct) #Windage in percentage (as a decimal.)
+    npra["status_code"]=status_in_water #Specify the particle state.
+    m = c_gnome.wind_mover((WindU,WindV)) #U,V velocity component in m/s.
+    
+    
+    #==========================================================================
+    #   Calculate the end coordinate analytically from the input parameters.
+    #==========================================================================
+
+    #Convert from Cartesian (U&V) to polar (magnitude and angle).
+    dist=np.hypot(WindU,WindV)*TotalT #(m/s)*s=distance in meters.
+    azimuth=np.mod(np.rad2deg(np.arctan2(WindU,WindV)),360.0) #Degrees clockwise from north.
+    
+    knownLat,knownLon=geodesic_calcs.reckon_GNOMEstyle(LatIn,LonIn,dist,azimuth) #return EndLat,EndLon
+    
+    #==========================================================================
+    #   Calculate the end coordinate in a single time-step via pyGNOME.
+    #==========================================================================
+
+    m.get_move(TotalT,npra) #Timestep(seconds),NumpyArray.
+    
+    #Ending coordinate.
+    EndLat=npra["p"]["p_lat"][0]
+    EndLon=npra["p"]["p_long"][0]
+    
+    #==========================================================================
+    #   Calculate the end coordinate over a series of time-steps via pyGNOME.
+    #==========================================================================
+    
+    npra["p"]=(LonIn,LatIn) #Set the point field of the numpy array back to the start coordinate.
+    Tsteps=np.tile(TotalT/Tdivisor,(Tdivisor,1)) #Create a vector of time-steps (in seconds) that evenly spans total run duration.
+    LatSteps=np.tile(np.nan,Tsteps.shape) #Preallocate vectors for subsequent loop.
+    LonSteps=np.tile(np.nan,Tsteps.shape)
+    
+    idx=-1
+    for iStep in Tsteps:
+        m.get_move(iStep,npra)
+        idx=idx+1
+        LatSteps[idx]=npra["p"]["p_lat"][0]
+        LonSteps[idx]=npra["p"]["p_long"][0]
+    
+    
+    
+    #==========================================================================
+    #   Input parameters defined above specify that the given particle be
+    #   moved [100%]*[1 m/s]*[1000 s] = 1 km due north.  Compare the end
+    #   coordinate reached by having pyGNOME divide this trajectory into a
+    #   series of (time) steps with the analytical solution.
+    #==========================================================================
+    
+    
+    
+    if np.allclose(LatSteps[-1],knownLat) and np.allclose(LonSteps[-1],knownLon) and np.allclose(LatSteps[-1],EndLat) and np.allclose(LonSteps[-1],EndLon):
+        print "pyGNOME latitude & longitude (%g, %g) at the end of %g time-steps are equivalent to both analytical latitude & longitude (%g, %g) and pyGNOME latitude & longitude (%g, %g) calculated with a single time-step." %(LatSteps[-1],LonSteps[-1],Tdivisor,knownLat,knownLon,EndLat,EndLon)
+    else:
+        print "Error:  pyGNOME latitude & longitude (%g, %g) at the end of %g time-steps are NOT equivalent to both analytical latitude & longitude (%g, %g) and pyGNOME latitude & longitude (%g, %g) calculated with a single time-step." %(LatSteps[-1],LonSteps[-1],Tdivisor,knownLat,knownLon,EndLat,EndLon)
+    
+    assert(np.allclose(LatSteps[-1],knownLat) and np.allclose(LonSteps[-1],knownLon) and np.allclose(LatSteps[-1],EndLat) and np.allclose(LonSteps[-1],EndLon)) #If the analytic and pyGNOME results differ too much, thow an error so the test fails.
