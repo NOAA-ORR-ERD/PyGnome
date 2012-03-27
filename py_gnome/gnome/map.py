ea211ec251b668dbbf69894ae353211f2d06234f
Alex Hadjilambris <alex.hadjilambris@orrw-lm-1691003.nos.noaa>
2012-03-27 13:44:32 -0700

    Added some documentation to map.py

--- py_gnome/gnome/map.py
+++ py_gnome/gnome/map.py
@@ -17,26 +17,31 @@ from hazpy.file_tools import haz_files
 from hazpy.geometry import polygons
 
 class gnome_map(map_canvas.MapCanvas):
-    
-    """basic color bitmap."""
-
-        
+    """
+        Basics pyGNOME color bitmap.
+        (End-user visualization.)
+    """     
     def __init__(self, image_size, bna_filename, color_mode='RGB'):
+        """
+            Initializes color map attributes. Calls on parent class initialization 
+            method in order to handle projection scaling.
+        """
         map_canvas.MapCanvas.__init__(self, image_size, projection=map_canvas.FlatEarthProjection, mode=color_mode)
         self.polygons = haz_files.ReadBNA(bna_filename, "PolygonSet")
         self.filename = bna_filename        
         self.draw_land(self.polygons)
 
-
-    def __del__(self):
-        pass        
-
     def to_pixel(self, coord):
+        """ Projects a (lon, lat) tuple onto the bitmap, and returns the resultant tuple. """
         coord = tuple(self.projection.to_pixel(np.array((coord[0], coord[1]))))
         coord = (int(coord[0]), int(coord[1]))
         return coord
         
     def to_pixel_array(self, coords):
+        """ 
+            Projects an array of (lon, lat) tuples onto the bitmap, and modifies it in 
+            place to hold the corresponding projected values.
+        """
         coords['p_long'] -= self.projection.center[0]
         coords['p_lat'] -= self.projection.center[1]
         coords['p_long'] *= self.projection.scale[0]
@@ -48,6 +53,7 @@ class gnome_map(map_canvas.MapCanvas):
     
     
     def _type(self):
+        """ This requires an explanation. """
         return ' color bitmap'
         
 class lw_map(gnome_map):
@@ -59,30 +65,41 @@ class lw_map(gnome_map):
     land_color = 1
     
     def __init__(self, image_size, bna_filename, refloat_halflife, color_mode='1'):
+        """
+            Initializes land-water map attributes. Calls on parent class initialization 
+            method in order to handle projection scaling. Caches its bounding_box so that
+            it doesn't need to be computed repeatedly.
+        """
         gnome_map.__init__(self, image_size, bna_filename, color_mode)
         self.bounding_box = self.polygons.bounding_box
         self.refloat_halflife = refloat_halflife
         self.spills = []
-    
-    def __del__(self):
-        pass
 
     def _type(self):
+        """ Returns the map type. (Either 'color' or 'land-water'.) """
         return ' land-water bitmap'
         
     def on_map(self, pixel_coord):
+        """ 
+            Given a tuple in pixel coordinates, determines whether the position is on the map.
+            It is actually not behaving correctly at the moment: the map bounds and the bounding box
+            may not necessarily coincide. Needs fixing!
+        """
         bounding_box = self.bounding_box
         if pixel_coord[0] > bounding_box[0][0] and pixel_coord[0] < bounding_box[1][0] and pixel_coord[1] > bounding_box[0][1] and pixel_coord[1] < bounding_box[1][1]:
             return True
         return False
 
     def on_land(self, coord):
+        """ Given lat-lon coordinates, determines whether the position is on land. """
         return not self.in_water(coord)
 
     def on_land_pixel(self, coord):
+        """ Given a tuple in pixel coordinates, determines whether the position is on land. """
         return not self.in_water_pixel(coord)
         
     def in_water(self, coord):
+        """ Given lat-lon coordinates, determines whether the position is in water. """
         coord = self.to_pixel(coord)
         if not self.on_map(coord):
             return False
@@ -97,6 +114,7 @@ class lw_map(gnome_map):
             return False
             
     def in_water_pixel(self, coord):
+        """ Given a tuple in pixel coordinates, determines whether the position is in water. """
         coord = coord.tolist()
         if not self.on_map(coord):
             return False
@@ -111,15 +129,28 @@ class lw_map(gnome_map):
             return False
 
     def allowable_spill_position(self, coord):
+        """ Determines whether a position given in lat-lon coordinates is an allowable spill location. """
         return self.in_water(coord)
         
     def set_spill(self, coord, num_particles, release_time):
+        """ 
+            Sets a spill.
+        ++args:
+            coord: (lon, lat)
+            release_time in seconds.
+        """
         if not self.allowable_spill_position(coord):
             print  "spill " + str(dict((('position', coord), ('num_particles', num_particles), ('release_time', release_time)))) + " ignored."
         else:
             self.spills += [(coord, num_particles, release_time)]
 
     def beach_element(self, p, lwp):
+        """ 
+            Beaches an element that has been landed.
+        ++args:
+            p: current position (see basic_types.world_point dtype)
+            lwp: last water position (see basic_types.world_point dtype)
+        """
         in_water = self.in_water
         displacement = ((p['p_long'] - lwp['p_long']), (p['p_lat'] - lwp['p_lat']))
         while not in_water((p['p_long'], p['p_lat'])):
