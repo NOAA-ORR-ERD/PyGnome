e9ad028594ec93b138909dfa3578bad360b944db
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2012-09-06 14:27:49 -0700

    Fixed ExportSymbols.h so it works with gui_gnome as before

--- lib_gnome/ExportSymbols.h
+++ lib_gnome/ExportSymbols.h
@@ -2,7 +2,11 @@
 	#ifdef LIB_GNOMEDLL_EXPORTS
 		#define  GNOMEDLL_API __declspec(dllexport)
 	#else
-		#define  GNOMEDLL_API __declspec(dllimport) 
+		#ifdef IBM
+			#define GNOMEDLL_API	// for gui_gnome to work correctly
+		#else
+			#define  GNOMEDLL_API __declspec(dllimport) 
+		#endif // IBM
 	#endif // LIB_GNOMEDLL_EXPORTS
 #else 
 	// for linux/mac
