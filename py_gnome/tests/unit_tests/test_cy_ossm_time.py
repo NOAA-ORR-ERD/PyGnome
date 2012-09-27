01711dbecb9cb9173cc283396ffdfffaf83442a9
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2012-09-27 15:04:48 -0700

    tests updated cy_ossm_time methods

--- py_gnome/tests/unit_tests/test_cy_ossm_time.py
+++ py_gnome/tests/unit_tests/test_cy_ossm_time.py
@@ -21,17 +21,6 @@ class TestCy_ossm_time():
     ossmT = cy_ossm_time.Cy_ossm_time()
     ossmT.ReadTimeValues(file); # assume default format and units
     
-    # use this to test setting / getting TimeValuePair
-    tval = np.empty((2,), dtype=basic_types.time_value_pair)
-    tval['time'][0] = 0
-    tval['value']['u'][0]=1
-    tval['value']['v'][0]=2
-    
-    tval['time'][1] = 1
-    tval['value']['u'][1]=2
-    tval['value']['v'][1]=3
-    
-    
     def test_ReadTimeValues(self):
         """
         Tests ReadTimeValues method. Use default format and units.
@@ -47,25 +36,28 @@ class TestCy_ossm_time():
         except IOError:
             assert True
     
-    def test_GetTimeValue(self):
-        """Test GetTimeValues method at model_time = 0"""
+    def test_TimeValuesAtDataPointsReadFromFile(self):
+        """Test GetTimeValues method. It gets the time value pairs for the model times
+        stored in the data file. 
+        For each line in the data file, the ReadTimeValues method creates one time value pair
+            This test just gets the time series that was created from the file. It then invokes
+        GetTimeValue for times in the time series.          
+        """
         
         # Let's see what is stored in the Handle to expected result
-        #t_val = self.ossmT.GetTimeValueHandle()
-        velrec = np.empty((1,), dtype=basic_types.velocity_rec)
-        velrec['u'] = 8.7448
-        velrec['v'] = 0
+        t_val = self.ossmT.GetTimeValueHandle()
         
-        vel_rec = np.empty((1,), dtype=basic_types.velocity_rec)
-        vel_rec = self.ossmT.GetTimeValue(0)
+        actual = np.array(t_val['value'], dtype=basic_types.velocity_rec)
+        time = np.array(t_val['time'], dtype=basic_types.seconds)
         
-        print vel_rec['u'], velrec['u'][0]
-        print vel_rec['v'], velrec['v'][0]
+        vel_rec = self.ossmT.GetTimeValue(time)
+        
         # TODO: Figure out why following fails??
-        #np.testing.assert_allclose(vel_rec, velrec[0], 1e-3, 1e-3, 
+        #np.testing.assert_allclose(vel_rec, actual, 1e-3, 1e-3, 
         #                          "GetTimeValue is not within a tolerance of 1e-3", 0)
-        assert np.abs( vel_rec['u']-velrec['u'][0]) < 1e-6
-        assert np.abs( vel_rec['v']-velrec['v'][0]) < 1e-6
+        assert np.all( np.abs( vel_rec['u']-actual['u'])) < 1e-6
+        assert np.all( np.abs( vel_rec['v']-actual['v'])) < 1e-6
+        
         
     def test_SetTimeValueHandle(self):
         """
@@ -73,8 +65,18 @@ class TestCy_ossm_time():
         array containing time_value_pair data
         It then reads it back to make sure data was set correctly
         """
-        self.ossmT.SetTimeValueHandle(self.tval)
+        # use this to test setting / getting TimeValuePair
+        tval = np.empty((2,), dtype=basic_types.time_value_pair)
+        tval['time'][0] = 0
+        tval['value']['u'][0]=1
+        tval['value']['v'][0]=2
+        
+        tval['time'][1] = 1
+        tval['value']['u'][1]=2
+        tval['value']['v'][1]=3
+        
+        self.ossmT.SetTimeValueHandle(tval)
         t_val = self.ossmT.GetTimeValueHandle()
-        np.testing.assert_array_equal(t_val, self.tval, 
+        np.testing.assert_array_equal(t_val, tval, 
                                       "cy_ossm_time.GetTimeValue did not return expected numpy array", 
                                       0)
