2690944155924447a2d42072789bc73bd592a8d6
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2012-09-28 13:08:52 -0700

    updated constructor and added error handling - however, this seg faults for a test right now

--- py_gnome/tests/unit_tests/test_cy_ossm_time.py
+++ py_gnome/tests/unit_tests/test_cy_ossm_time.py
@@ -18,23 +18,28 @@ class TestCy_ossm_time():
     """
     # sample data generated and stored via Gnome GUI
     file = r"SampleData/WindDataFromGnome.WND"
-    ossmT = cy_ossm_time.Cy_ossm_time()
-    ossmT.ReadTimeValues(file); # assume default format and units
+    ossmT = cy_ossm_time.Cy_ossm_time(path=file,
+                                      file_contains=basic_types.file_contains.magnitude_direction,
+                                      units=basic_types.velocity_units.knots)
     
-    def test_ReadTimeValues(self):
-        """
-        Tests ReadTimeValues method. Use default format and units.
-        """
-        print "Read file " + self.file
-        assert True
-    
-    def test_ReadTimeValuesException(self):
-        """Test error when ReadTimeValues does not provide units in data file or as input"""
-        ossmT2 = cy_ossm_time.Cy_ossm_time()
+    def test_initExceptions(self):
+        """Test exceptions during __init__ """
+        
+        # no inputs
         try:
-            ossmT2.ReadTimeValues(self.file, 5, -1)
-        except IOError:
+            ossmT2 = cy_ossm_time.Cy_ossm_time()
+        except ValueError as e:
+            print(e)
             assert True
+            
+        # bad path
+        try:
+            file = r"SampleData/WindDataFromGnome.WNDX"
+            ossmT2 = cy_ossm_time.Cy_ossm_time(path=file, file_contains=basic_types.file_contains.magnitude_direction)
+        except IOError as e:
+            print(e)
+            assert True
+    
     
     def test_TimeValuesAtDataPointsReadFromFile(self):
         """Test GetTimeValues method. It gets the time value pairs for the model times
@@ -43,29 +48,33 @@ class TestCy_ossm_time():
             This test just gets the time series that was created from the file. It then invokes
         GetTimeValue for times in the time series.          
         """
-        
         # Let's see what is stored in the Handle to expected result
-        t_val = self.ossmT.GetTimeValueHandle()
+        t_val = self.ossmT.Timeseries() 
+        #print t_val
+        #assert False
         
         actual = np.array(t_val['value'], dtype=basic_types.velocity_rec)
         time = np.array(t_val['time'], dtype=basic_types.seconds)
         
         vel_rec = self.ossmT.GetTimeValue(time)
-        
         # TODO: Figure out why following fails??
         #np.testing.assert_allclose(vel_rec, actual, 1e-3, 1e-3, 
         #                          "GetTimeValue is not within a tolerance of 1e-3", 0)
-        assert np.all( np.abs( vel_rec['u']-actual['u'])) < 1e-6
-        assert np.all( np.abs( vel_rec['v']-actual['v'])) < 1e-6
+        tol = 1e-6
+        np.testing.assert_allclose(vel_rec['u'], actual['u'], tol, tol, 
+                                  "GetTimeValue is not within a tolerance of "+str(tol), 0)
+        np.testing.assert_allclose(vel_rec['v'], actual['v'], tol, tol, 
+                                  "GetTimeValue is not within a tolerance of "+str(tol), 0)
+        #assert np.all( np.abs( vel_rec['u']-actual['u'])) < 1e-6
+        #assert np.all( np.abs( vel_rec['v']-actual['v'])) < 1e-6
         
         
-    def test_SetTimeValueHandle(self):
+    def test_initFromTimeSeries(self):
         """
         Sets the time series in OSSMTimeValue_c equal to the externally supplied numpy
         array containing time_value_pair data
         It then reads it back to make sure data was set correctly
         """
-        # use this to test setting / getting TimeValuePair
         tval = np.empty((2,), dtype=basic_types.time_value_pair)
         tval['time'][0] = 0
         tval['value']['u'][0]=1
@@ -75,8 +84,15 @@ class TestCy_ossm_time():
         tval['value']['u'][1]=2
         tval['value']['v'][1]=3
         
-        self.ossmT.SetTimeValueHandle(tval)
-        t_val = self.ossmT.GetTimeValueHandle()
-        np.testing.assert_array_equal(t_val, tval, 
-                                      "cy_ossm_time.GetTimeValue did not return expected numpy array", 
-                                      0)
+        ossm = cy_ossm_time.Cy_ossm_time(timeseries=tval, units=basic_types.velocity_units.knots)
+        #t_val = ossmT.Timeseries()
+        
+        self.ossmT._SetTimeValueHandle(tval)
+        
+        #t_val = self.ossmT.Timeseries()
+#        np.testing.assert_array_equal(t_val, tval, 
+#                                      "cy_ossm_time.GetTimeValue did not return expected numpy array", 
+#                                      0)
+        print t_val
+        #print actual
+        assert False
