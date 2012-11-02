3a104fc24581d9588d8a7cd1eaf1b33028d997f7
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2012-11-02 14:37:04 -0700

    Fixed #78. There is only one prepare_for_model_step(...)

--- py_gnome/gnome/cy_gnome/cy_wind_mover.pyx
+++ py_gnome/gnome/cy_gnome/cy_wind_mover.pyx
@@ -49,7 +49,7 @@ cdef class CyWindMover:
         """
         self.mover.PrepareForModelRun()
         
-    def prepare_for_model_step(self, model_time, step_len, uncertain):
+    def prepare_for_model_step(self, model_time, step_len, numSets=0, np.ndarray[np.npy_int] setSizes=None):
         """
         .. function:: prepare_for_model_step(self, model_time, step_len, uncertain)
         
@@ -57,30 +57,13 @@ cdef class CyWindMover:
         
         :param model_time: current model time.
         :param step_len: length of the time step over which the get move will be computed
-        :param uncertain: bool flag determines whether to apply uncertainty or not
         """
         cdef OSErr err
-        err = self.mover.PrepareForModelStep(model_time, step_len, uncertain, 0, NULL)
-        if err != 0:
-            """
-            For now just raise an OSError - until the types of possible errors are defined and enumerated
-            """
-            raise OSError("WindMover_c.PreareForModelStep returned an error.")
-
-    def prepare_for_model_step_uncertain(self, model_time, step_len, uncertain, numSets, np.ndarray[np.npy_int] setSizes):
-        """
-        .. function:: prepare_for_model_step(self, model_time, step_len, uncertain)
-        
-        prepares the mover for time step, calls the underlying C++ mover objects PrepareForModelStep(..)
-        
-        :param model_time: 
-        :param step_len:
-        :param uncertain: bool flag determines whether to apply uncertainty or not
-        """
-        cdef OSErr err
-        numSets = len(setSizes) 
-
-        err = self.mover.PrepareForModelStep(model_time, step_len, uncertain, numSets, <int *>&setSizes[0])
+        if numSets == 0:
+            err = self.mover.PrepareForModelStep(model_time, step_len, False, 0, NULL)
+        else:
+            err = self.mover.PrepareForModelStep(model_time, step_len, True, numSets, <int *>&setSizes[0])
+            
         if err != 0:
             """
             For now just raise an OSError - until the types of possible errors are defined and enumerated
