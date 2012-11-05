1a180a28959b0ab6f7b2f89a4e4c1ab4767628e2
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2012-11-05 08:57:48 -0800

    WindMover(..) only inherits from PyMover object - PyMover defines default methods for prepare_for_model* - PyMover expects child to define a member called mover that implements these prepare_for_model_run and prepare_for_model_step methods - updated tests so they use datetime object for model_time

--- py_gnome/tests/unit_tests/test_wind_mover.py
+++ py_gnome/tests/unit_tests/test_wind_mover.py
@@ -8,6 +8,7 @@ from gnome.utilities import projections
 import numpy as np
 
 import datetime
+from datetime import timedelta
 import pytest
 
 
@@ -32,8 +33,7 @@ class TestWindMover():
     start_pos += (3.,6.,0.)
     rel_time = datetime.datetime(2012, 8, 20, 13)    # yyyy/month/day/hr/min/sec
     #model_time = basic_types.dt_to_epoch(rel_time)    # TODO: should this happen in mover or in model?
-    model_time_sec = time_utils.date_to_sec(rel_time) + 1
-    model_time = time_utils.sec_to_date(model_time_sec)
+    model_time = time_utils.sec_to_date(time_utils.date_to_sec(rel_time) + 1)
     time_step = 15*60 # seconds
 
     pSpill = spill.PointReleaseSpill(num_le, start_pos, rel_time, persist=-1)
@@ -53,12 +53,37 @@ class TestWindMover():
 
     def test_get_move(self):
         """
+        Test the get_move(...) results in WindMover match the expected delta
         """
-        self.pSpill.prepare_for_model_step(self.model_time, self.time_step, self.pSpill.is_uncertain)
-        self.wm.prepare_for_model_step(self.model_time_sec, self.time_step, self.pSpill.is_uncertain)
+        self.pSpill.prepare_for_model_step(self.model_time, self.time_step)
+        self.wm.prepare_for_model_step(self.model_time, self.time_step)
 
-        delta = self.wm.get_move(self.pSpill, self.time_step, self.model_time_sec)
-
+        # make sure clean up is happening fine
+        delta = np.zeros((2,self.pSpill.num_LEs), dtype=basic_types.world_point)
+        delta = np.zeros((2,self.pSpill.num_LEs), dtype=basic_types.world_point)
+        actual =np.zeros((2,self.pSpill.num_LEs), dtype=basic_types.world_point) 
+        for ix in range(0,2):
+            curr_time = time_utils.sec_to_date(time_utils.date_to_sec(self.model_time)+(self.time_step*ix))
+            print "Time step [sec]: " + str( time_utils.date_to_sec(curr_time)-time_utils.date_to_sec(self.model_time))
+            delta[ix] = self.wm.get_move(self.pSpill, self.time_step, curr_time)
+            
+            actual[ix] = self._expected_move()
+            
+        # the results should be independent of model time
+        tol = 1e-8
+        print "C++ delta-lat: ";   print str(delta['lat'])
+        print "Expected delta-lat: "; print str(actual['lat'])
+        print "C++ delta-long: ";  print str(delta['long'])
+        print "Expected delta-long: ";print str(actual['long'])
+        np.testing.assert_allclose(delta['lat'], actual['lat'], tol, tol,
+                                   "get_time_value is not within a tolerance of " + str(tol), 0)
+        np.testing.assert_allclose(delta['long'], actual['long'], tol, tol,
+                               "get_time_value is not within a tolerance of "+str(tol), 0)
+       
+    def _expected_move(self):
+        """
+        Put the expected move logic in separate private method
+        """
         # expected move
         exp = np.zeros( (self.pSpill.num_LEs, 3) )
         exp[:,0] = self.pSpill['windages']*self.time_val[0]['value']['u']*self.time_step # 'u'
@@ -69,17 +94,10 @@ class TestWindMover():
         actual = np.zeros((len(exp),), dtype=basic_types.world_point)
         actual ['lat'] = xform[:, 1]
         actual ['long'] = xform[:, 0]
-
-        tol = 1e-8
-        print "C++ lat: " + "\t" + str(delta['lat'])
-        print "check lat: " + "\t" + str(actual['lat'])
-        print "C++ long: " + "\t" + str(delta['long'])
-        print "check long: " + "\t" + str(actual['long'])
-        np.testing.assert_allclose(delta['lat'], actual['lat'], tol, tol,
-                                   "get_time_value is not within a tolerance of " + str(tol), 0)
-        np.testing.assert_allclose(delta['long'], actual['long'], tol, tol,
-                               "get_time_value is not within a tolerance of "+str(tol), 0)
-       
+        
+        return actual
+        
+        
        
 if __name__=="__main__":
     tw = TestWindMover()
