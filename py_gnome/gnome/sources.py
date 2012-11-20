a5bbc80465c13fb96436ad66e53ee81faa55c99c
chris.barker <chris.barker@noaa.gov>
2012-11-19 16:35:54 -0800

    added new files for new "only one spill" approach

---
+++ py_gnome/gnome/sources.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""
+sources.py
+
+classes for various types of sources of elements for GNOME
+
+these are managed by the spill class
+"""
+
+class element_source(object):
+    pass
