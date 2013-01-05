e4495163d04e7182d6883cc6aaed9caeebbf49d9
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2013-01-04 19:01:16 -0800

    moved time_utils tests to separate module

--- py_gnome/tests/unit_tests/test_cy_helpers.py
+++ py_gnome/tests/unit_tests/test_cy_helpers.py
@@ -24,44 +24,6 @@ date_rec =   np.dtype([('year', np.short),
                        ('second', np.short), 
                        ('dayOfWeek', np.short),], align=True)
 
-
-def _convert(x):
-    """
-    helper method for the next 4 tests
-    """
-    y = time_utils.date_to_sec(x)
-    xn= time_utils.sec_to_date(y)
-    return xn
-
-def test_scalar_input():
-    """
-    test time_utils conversion return a scalar if that is what the user input
-    
-    always returns a numpy object
-    """
-    x = datetime.now()
-    xn = _convert(x)
-    assert type(xn) == datetime
-    x = time_utils.round_time(x, roundTo=1)
-    assert type(x) == datetime 
-    assert x == xn
-    
-def test_datetime_array():
-    """
-    test time_utils conversion works for python datetime object
-    """
-    x = np.zeros((3,), dtype=datetime)
-    xn = _convert(x)
-    assert np.all( time_utils.round_time(x, roundTo=1) == xn)
-
-def test_numpy_array():
-    """
-    time_utils works for numpy datetime object
-    """
-    x = np.zeros((3,), dtype='datetime64[s]')
-    xn = _convert(x)
-    assert np.all( x == xn)
-
 class TestCyDateTime():
     target = cy_helpers.CyDateTime()
     now =  datetime.now()
