b6474a6083af1f11fbe75a49d227e52b6e7b76e6
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2013-01-18 16:58:47 -0800

    (WIP Cython refactor) updated cython movers to use base class

--- py_gnome/gnome/cy_gnome/cy_gridwind_mover.pyx
+++ py_gnome/gnome/cy_gnome/cy_gridwind_mover.pyx
@@ -1,34 +1,37 @@
-import cython
 cimport numpy as np
 import numpy as np
 
 from type_defs cimport *
-from movers cimport GridWindMover_c
-from movers cimport TimeGridVel_c
-from movers cimport TimeGridVelRect_c
-from movers cimport TimeGridVelCurv_c
-from movers cimport TimeGridWindRect_c
-from movers cimport TimeGridWindCurv_c
+from movers cimport Mover_c,GridWindMover_c,TimeGridVel_c,TimeGridVelRect_c,TimeGridVelCurv_c,TimeGridWindRect_c,TimeGridWindCurv_c
+cimport cy_mover
+"""
+Dynamic casts are not currently supported in Cython - define it here instead.
+Since this function is custom for each mover, just keep it with the definition for each mover
+"""
+cdef extern from *:
+    GridWindMover_c* dynamic_cast_ptr "dynamic_cast<GridWindMover_c *>" (Mover_c *) except NULL
+    
+cdef class CyGridWindMover(cy_mover.CyMover):
 
-cdef class CyGridWindMover:
-
-    cdef GridWindMover_c *mover
+    cdef GridWindMover_c *grid
     
     def __cinit__(self):
         self.mover = new GridWindMover_c()
+        self.grid = dynamic_cast_ptr(self.mover)
     
     def __dealloc__(self):
         del self.mover
+        self.grid = NULL
     
 #     def set_time_grid(self, time_grid_file, topology_file):
-#         self.mover.fIsOptimizedForStep = 0
+#         self.grid.fIsOptimizedForStep = 0
 #         #cdef TimeGridVel_c *time_grid
 #         cdef TimeGridVelCurv_c *time_grid
 #         time_grid = new TimeGridVelCurv_c()
 #         #time_grid = new TimeGridVel_c()
 #         if (time_grid.TextRead(time_grid_file, topology_file) == -1):
 #             return False
-#         self.mover.SetTimeGrid(time_grid)
+#         self.grid.SetTimeGrid(time_grid)
 #         return True
             
 
@@ -38,7 +41,7 @@ cdef class CyGridWindMover:
         
         """
         cdef OSErr err
-        err = self.mover.TextRead(time_grid_file, topology_file)
+        err = self.grid.TextRead(time_grid_file, topology_file)
         if err != 0:
             """
             For now just raise an OSError - until the types of possible errors are defined and enumerated
@@ -46,35 +49,7 @@ cdef class CyGridWindMover:
             raise OSError("GridWindMover_c.TextRead returned an error.")
 
     def __init__(self):
-        self.mover.fIsOptimizedForStep = 0
-
-    def prepare_for_model_run(self):
-        """
-        .. function::prepare_for_model_run
-        
-        """
-        self.mover.PrepareForModelRun()
-        
-    def prepare_for_model_step(self, model_time, step_len, numSets=0, np.ndarray[np.npy_int] setSizes=None):
-        """
-        .. function:: prepare_for_model_step(self, model_time, step_len, uncertain)
-        
-        prepares the mover for time step, calls the underlying C++ mover objects PrepareForModelStep(..)
-        
-        :param model_time: current model time.
-        :param step_len: length of the time step over which the get move will be computed
-        """
-        cdef OSErr err
-        if numSets == 0:
-            err = self.mover.PrepareForModelStep(model_time, step_len, False, 0, NULL)
-        else:
-            err = self.mover.PrepareForModelStep(model_time, step_len, True, numSets, <int *>&setSizes[0])
-            
-        if err != 0:
-            """
-            For now just raise an OSError - until the types of possible errors are defined and enumerated
-            """
-            raise OSError("GridWindMover_c.PrepareForModelStep returned an error.")
+        self.grid.fIsOptimizedForStep = 0
 
     def get_move(self, 
                  model_time, 
@@ -113,7 +88,7 @@ cdef class CyGridWindMover:
         cdef OSErr err
         N = len(ref_points) 
 
-        err = self.mover.get_move(N, model_time, step_len, &ref_points[0], &delta[0], &windages[0], <short *>&LE_status[0], spill_type, spill_ID)
+        err = self.grid.get_move(N, model_time, step_len, &ref_points[0], &delta[0], &windages[0], <short *>&LE_status[0], spill_type, spill_ID)
         if err == 1:
             raise ValueError("Make sure numpy arrays for ref_points and delta are defined")
         
@@ -122,10 +97,3 @@ cdef class CyGridWindMover:
         """
         if err == 2:
             raise ValueError("The value for spill type can only be 'forecast' or 'uncertainty' - you've chosen: " + str(spill_type))
-        
-
-    def model_step_is_done(self):
-        """
-        invoke C++ model step is done functionality
-        """
-        self.mover.ModelStepIsDone()
--- py_gnome/gnome/cy_gnome/movers.pxd
+++ py_gnome/gnome/cy_gnome/movers.pxd
@@ -146,17 +146,14 @@ cdef extern from "GridCurrentMover_c.h":
         float    fMaxDepthForExtrapolation
         
         GridCurrentMover_c ()
-        WorldPoint3D        GetMove(Seconds&,Seconds&,Seconds&,Seconds&, long, long, LERec *, LETYPE)
-        OSErr 		PrepareForModelRun()
-        OSErr 		get_move(int n, unsigned long model_time, unsigned long step_len, WorldPoint3D* ref, WorldPoint3D* delta, short* LE_status, LEType spillType, long spillID)
-        OSErr 		PrepareForModelStep(Seconds&, Seconds&, bool, int numLESets, int* LESetsSizesList)
-        void 		ModelStepIsDone()
-        void 		SetTimeGrid(TimeGridVel_c *newTimeGrid)
-        OSErr                TextRead(char *path,char *topFilePath)
+        WorldPoint3D    GetMove(Seconds&,Seconds&,Seconds&,Seconds&, long, long, LERec *, LETYPE)
+        OSErr 		    get_move(int n, unsigned long model_time, unsigned long step_len, WorldPoint3D* ref, WorldPoint3D* delta, short* LE_status, LEType spillType, long spillID)
+        void 		    SetTimeGrid(TimeGridVel_c *newTimeGrid)
+        OSErr           TextRead(char *path,char *topFilePath)
         
 cdef extern from "GridWindMover_c.h":
     
-    cdef cppclass GridWindMover_c:
+    cdef cppclass GridWindMover_c(WindMover_c):
         TimeGridVel_c    *timeGrid
         Boolean fIsOptimizedForStep
         float    fWindScale
@@ -164,11 +161,7 @@ cdef extern from "GridWindMover_c.h":
         short    fUserUnits
         
         GridWindMover_c ()
-        WorldPoint3D        GetMove(Seconds&,Seconds&,Seconds&,Seconds&, long, long, LERec *, LETYPE)
-        OSErr 		PrepareForModelRun()
-        OSErr 		get_move(int n, unsigned long model_time, unsigned long step_len, WorldPoint3D* ref, WorldPoint3D* delta, double* windages, short* LE_status, LEType spillType, long spillID)
-        OSErr 		PrepareForModelStep(Seconds&, Seconds&, bool, int numLESets, int* LESetsSizesList)
-        void 		ModelStepIsDone()
-        void 		SetTimeGrid(TimeGridVel_c *newTimeGrid)
-        OSErr                TextRead(char *path,char *topFilePath)
+        WorldPoint3D    GetMove(Seconds&,Seconds&,Seconds&,Seconds&, long, long, LERec *, LETYPE)
+        void 		    SetTimeGrid(TimeGridVel_c *newTimeGrid)
+        OSErr           TextRead(char *path,char *topFilePath)
         
