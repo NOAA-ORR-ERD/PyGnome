54cf7d44c4ad0281f3338009b2501c130cc8432c
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2013-02-11 13:21:56 -0800

    added function to get tsformat from string. Also updated convert methods to take string inputs and added tests

--- py_gnome/gnome/utilities/convert.py
+++ py_gnome/gnome/utilities/convert.py
@@ -13,13 +13,17 @@ def to_time_value_pair(datetime_value_2d, in_ts_format):
     into a time_value_pair array 
     
     :param datetime_value_2d: numpy array of type basic_types.datetime_value_2d
-    :param in_ts_format: format of the array defined by one of the options given in basic_types.ts_format
+    :param in_ts_format: format of the array. Can be defined by a string 'r-theta', 'uv' or by an integer
+                         defined by one of the options given in basic_types.ts_format
     """
     if datetime_value_2d.dtype is not basic_types.datetime_value_2d:
         raise ValueError("Method expects a numpy array containing basic_types.datetime_value_2d")
     
     # convert datetime_value_2d to time_value_pair
     time_value_pair = np.zeros((len(datetime_value_2d),), dtype=basic_types.time_value_pair)
+    
+    if type(in_ts_format) is str:
+        in_ts_format = tsformat(in_ts_format)
     
     if in_ts_format == basic_types.ts_format.magnitude_direction:
         time_value_pair['time'] = time_utils.date_to_sec(datetime_value_2d['time'])
@@ -51,6 +55,9 @@ def to_datetime_value_2d(time_value_pair, out_ts_format):
     
     datetime_value_2d = np.zeros((len(time_value_pair),), dtype=basic_types.datetime_value_2d)
     
+    if type(out_ts_format) is str:
+        out_ts_format = tsformat(out_ts_format)
+        
     # convert time_value_pair to datetime_value_2d in desired output format
     if out_ts_format == basic_types.ts_format.magnitude_direction:
         datetime_value_2d['time'] = time_utils.sec_to_date(time_value_pair['time'])
@@ -71,3 +78,14 @@ def to_datetime_value_2d(time_value_pair, out_ts_format):
     
     
     return datetime_value_2d
+
+def tsformat(format):
+    """
+    convert string 'uv' or 'magnitude_direction' into appropriate integer given by basic_types.ts_format.*
+    """
+    if format == 'r-theta':
+        return basic_types.ts_format.magnitude_direction
+    elif format == 'uv':
+        return basic_types.ts_format.uv
+    else:
+        raise ValueError("timeseries format can only be 'r-theta' or 'uv', the format entered is not recognized as valid format")
