fe203016434113e580c415ac817cd4f80be3672d
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2013-02-14 15:38:11 -0800

    commented set method for filename property for now

--- py_gnome/gnome/cy_gnome/cy_ossm_time.pyx
+++ py_gnome/gnome/cy_gnome/cy_ossm_time.pyx
@@ -93,15 +93,16 @@ cdef class CyOSSMTime(object):
         def __get__(self):
             return <bytes>self.time_dep.fileName
         
-        def __set__(self, value):
-            max_ = cython.sizeof(self.time_dep.fileName)
-            if len(value) > max_:
-                print "Only first 256 char will be written: {0}\n".format(value[:max_])
-                
-            for i, char_ in enumerate(value):
-                print i, char_
-                self.time_dep.fileName[i] = char_
-                #self.time_dep.fileName[i] = char_
+        #=======================================================================
+        # def __set__(self, value):
+        #    max_ = cython.sizeof(self.time_dep.fileName)
+        #    if len(value) > max_:
+        #        print "Only first 256 char will be written: {0}\n".format(value[:max_])
+        #        
+        #    for i, char_ in enumerate(value):
+        #        print i, char_
+        #        self.time_dep.fileName[i] = 'char_'
+        #=======================================================================
         
     property station_location:
         def __get__(self):
