1b2596c7657f815a3d8df7db68451ea44554d01e
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2013-02-25 16:33:11 -0800

    fixed WindMover to use numpy.count_nonzero to count elements in mask

--- py_gnome/gnome/movers/movers.py
+++ py_gnome/gnome/movers/movers.py
@@ -296,7 +296,7 @@ class WindMover(CyMover):
                                                                       spill.windage_range[1],
                                                                       spill.windage_persist,
                                                                       time_step,
-                                                                      array_len=len(np.where(spill_mask)[0]))
+                                                                      array_len=np.count_nonzero(spill_mask))
             if sc.uncertain:
                 WindMover._uspill_windage_is_set = True
             else:
