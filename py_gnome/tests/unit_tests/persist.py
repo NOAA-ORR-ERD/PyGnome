7bd34bd0d379f9b6a62820912023dae205c2b457
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2013-03-07 17:50:18 -0800

    updated test script to use new stuff

--- py_gnome/tests/unit_tests/persist.py
+++ py_gnome/tests/unit_tests/persist.py
@@ -1,48 +1,42 @@
-import datetime
-import time
 import json
+from datetime import datetime,timedelta
+import pprint
 
-import gnome.basic_types
-from gnome import environment
-from gnome.persist import environment_schema
+import numpy as np
+import colander
 
-"""
-helper functions to dump/load json or json like data to file
-"""
-def save(data, fname):
-    with open(fname,'w') as outfile:
-        json.dump(data, outfile, indent = True)
-
-def load(fname):
-    with open(fname,'r') as infile:
-        return json.load(infile)
+import gnome
+from gnome import movers
+from gnome.persist import scenario
 
 """
-Test case, Wind object 
-state to be saved in text file in json format
+Define a scenario and persist it to ./test_persist/
 """
-f =r'/Users/jasmine.sandhu/Documents/projects/gnome/py_gnome/tests/unit_tests/SampleData/WindDataFromGnome.WND'
-w = environment.Wind(file=f)
-#w.latitude = 10
-#w.longitude = 100
+saveloc = './test_persist'
+start_time = datetime(2013, 2, 13, 9, 0)
+model = gnome.model.Model(start_time = start_time,
+                        duration = timedelta(days=2),
+                        time_step = 30 * 60, # 1/2 hr in seconds
+                        uncertain = False,
+                        )
 
+#need a scenario for SimpleMover
+#model.movers += movers.simple_mover.SimpleMover(velocity=(1.0, -1.0, 0.0))
+model.movers += gnome.movers.RandomMover(diffusion_coef=100000)
 
-"""
-============================
-Use Colander to save state - 
-this is a possible approach if monkey-patch is successful for colander
-"""
-savfile_ = 'cdata.txt'
+series = np.array( (start_time, ( 10,   45) ),  dtype=gnome.basic_types.datetime_value_2d).reshape((1,))
+model.environment += gnome.environment.Wind(timeseries=series, units='meter per second')
 
-"""
-Using colander to serialize to json then save data in cdata.txt
-NOTE: this is not valid json format since everything is a string
-"""
-save( environment_schema.CreateWind().serialize(w.to_dict('create')), savfile_)
+model.movers += gnome.movers.WindMover( [w for w in model.environment][0] )
 
-"""
-Load json-like dict from file, deserialize and validate
-Create new object from dict
-"""
-c_dict = environment_schema.CreateWind().deserialize(load(savfile_) )# must deserialize through schema
-new_w1 = environment.Wind.new_from_dict(c_dict)                     # now use dict to create new object
+#===============================================================================
+# print "serializing .."
+# m_json = gnome.persist.model_schema.CreateModel().serialize( model.to_dict('create'))
+# print "deserializing .."
+# gnome.persist.model_schema.CreateModel().deserialize(m_json)
+#===============================================================================
+
+print "saving .."
+scenario.save(model,saveloc)
+# Load is currently not working
+#scenario.load(saveloc,'model_{0}.txt'.format(model.id))
