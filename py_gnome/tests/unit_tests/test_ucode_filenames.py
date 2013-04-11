6a9d09bd7f26ac8afca4ff3962b6c26ace73cabe
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2013-04-11 16:55:52 -0700

    added to_bytes() conversion in cython files with some tests

--- py_gnome/tests/unit_tests/test_ucode_filenames.py
+++ py_gnome/tests/unit_tests/test_ucode_filenames.py
@@ -9,9 +9,7 @@ import sys
 
 import pytest 
 
-from gnome import environment 
-
-datadir = os.path.join(os.path.dirname(__file__), r"SampleData")
+from gnome import environment, movers
 
 def create_ucode_file(filename):
     """
@@ -24,19 +22,66 @@ def create_ucode_file(filename):
     shutil.copyfile( filename, ufile )
     return ufile
 
-def test_ucode_char_in_filename():
+
+# join datadir to path in obj_ inside the test
+datadir = os.path.join(os.path.dirname(__file__), r"SampleData")
+obj_ = [(environment.Wind, r'WindDataFromGnome.WND'),
+        (environment.Tide, os.path.join('tides',r'CLISShio.txt')),
+        (environment.Tide, os.path.join('tides',r'TideHdr.FINAL'))]
+
+@pytest.mark.parametrize("test_case", obj_)
+def test_ucode_char_in_filename(test_case):
     # on windows
-    file_ = os.path.join(datadir, u'WindDataFromGnome.WND')
-    environment.Wind(filename=file_)
+    file_ = os.path.join(datadir, test_case[1])
     ufile = create_ucode_file(file_)
+    
+    # first check that it works with file without adding special character to filename
+    test_case[0](filename=file_)
     
     if sys.platform == 'win32':
         with pytest.raises(UnicodeEncodeError):
-            environment.Wind(filename=ufile)
+            test_case[0](filename=ufile)
     elif sys.platform == 'darwin':
-        environment.Wind(filename=ufile)
+        test_case[0](filename=ufile)
     else:
         with pytest.raises(NotImplementedError):
-            environment.Wind(filename=ufile)
+            test_case[0](filename=ufile)
     
+    print "{0}({1}) passed the test".format(test_case[0], test_case[1])
+    assert True
+
+
+gridmover_ = [(movers.GridCurrentMover, os.path.join(r'currents',r'ny_cg.nc'), None),
+              (movers.GridCurrentMover, os.path.join(r'winds',r'test_wind.cdf'), os.path.join(r'winds',r'WindSpeedDirSubsetTop.DAT')),
+              (movers.GridWindMover, os.path.join(r'currents',r'ny_cg.nc'), os.path.join(r'currents',r'NYTopology.dat')),
+              ]
+
+@pytest.mark.parametrize("mover_test", gridmover_)
+def test_ucode_char_in_grid_mover_filename(mover_test):
+    # on windows
+    file1_ = os.path.join(datadir, mover_test[1])
+    ufile1 = create_ucode_file(file1_)
+    
+    if mover_test[2] is None:
+        file2_ = None
+        ufile2 = None
+    else:
+        file2_ = os.path.join(datadir, mover_test[2])
+        ufile2 = create_ucode_file(file2_)
+    
+    # first check that it works with file without adding special character to filename
+    print mover_test
+    mover_test[0](file1_, file2_)
+    
+    if sys.platform == 'win32':
+        with pytest.raises(UnicodeEncodeError):
+            mover_test[0](ufile1, ufile2)
+    elif sys.platform == 'darwin':
+        mover_test[0](ufile1, ufile2)
+    else:
+        with pytest.raises(NotImplementedError):
+            mover_test[0](ufile1, ufile2)
+    
+    print "{0}({1},{2}) passed the test".format(mover_test[0], mover_test[1], mover_test[2])
+    assert True
     
