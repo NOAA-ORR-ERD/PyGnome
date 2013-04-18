d72aaf72c0d3252d118943e1a14ff61367d255d5
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2013-04-18 13:38:58 -0700

    added constants in gnomeobject to keep gnome constants

--- py_gnome/gnome/__init__.py
+++ py_gnome/gnome/__init__.py
@@ -8,7 +8,7 @@ __init__.py for the gnome package
 # a few imports so that the basic stuff is there
 
 #from gnomeobject import GnomeObject
-from gnomeobject import GnomeId
+from gnomeobject import GnomeId,constants
 from . import map
 from . import spill
 from . import spill_container
