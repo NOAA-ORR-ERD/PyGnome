ca0dfe853917c00b7d5b23abf8b48ed2cfbd462a
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2013-05-01 16:22:29 -0700

    added kwargs to prepare_for_model_run to account for different information for differet outputters

--- py_gnome/gnome/renderer.py
+++ py_gnome/gnome/renderer.py
@@ -90,7 +90,7 @@ class Renderer(Outputter, MapCanvas, serializable.Serializable):
     def images_dir_to_dict(self):
         return os.path.abspath(self.images_dir)
     
-    def prepare_for_model_run(self, cache=None):
+    def prepare_for_model_run(self, cache=None, **kwargs):
         """
         prepares the renderer for a model run.
 
@@ -99,8 +99,10 @@ class Renderer(Outputter, MapCanvas, serializable.Serializable):
         If you want to save the previous images, a new output dir should be set.
 
         :param cache=None: Sets the cache object to be used for the data.
-                           If None, it will use teh one already set up.
-
+                           If None, it will use the one already set up.
+        
+        Does not take anyother input arguments; however, to keep the interface the same for all outputters,
+        define **kwargs for now.
         """
         if cache is not None:
             self.cache = cache
@@ -139,6 +141,9 @@ class Renderer(Outputter, MapCanvas, serializable.Serializable):
         image_filename = os.path.join(self.images_dir, self.foreground_filename_format%step_num)
 
         self.create_foreground_image()
+
+        if self.cache is None:
+            raise ValueError("cache object is not defined. It is required prior to calling write_output")
 
         # pull the data from cache:
         for sc in self.cache.load_timestep(step_num).items():
