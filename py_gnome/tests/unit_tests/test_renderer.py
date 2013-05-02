1e4f17740b99ebd1c0f32c29a18aefbaaabf7358
chris.barker <chris.barker@noaa.gov>
2013-05-02 16:02:23 -0700

    added code to draw elements as Xs when they are on land.

--- py_gnome/tests/unit_tests/test_renderer.py
+++ py_gnome/tests/unit_tests/test_renderer.py
@@ -3,17 +3,24 @@
 """
 test code for renderer
 
-NOTE: some of the test that should be here are in test_map_canvas
+NOTE: some of these only really test if the code crashes
+  -- whether the rendering looks right -- who knows?
+  -- It's a good idea to look at the output.
 
 """
-import os, shutil
-
 import pytest
 
+import os, shutil
+
+import numpy.random as random
+
+from gnome import basic_types
 from gnome import renderer
+from gnome.spill_container import TestSpillContainer
 
 bna_sample = "SampleData/MapBounds_2Spillable2Islands2Lakes.bna"
-output_dir = "temp_output_dir"
+output_dir = os.path.join(os.path.dirname(__file__), r"renderer_output_dir")
+data_dir = os.path.join(os.path.dirname(__file__), r"SampleData")
 
 if os.path.isdir(output_dir):
     shutil.rmtree(output_dir)
@@ -44,8 +51,91 @@ def test_file_delete():
     files = os.listdir(output_dir)
     assert files == [r.background_map_name]
 
+def test_render_elements():
+    """
+    Should this test be in map_cnavas?
+    """
+    r = renderer.Renderer(os.path.join(data_dir, 'MapBounds_2Spillable2Islands2Lakes.bna'),
+                          output_dir,
+                          image_size=(800,600))
+    BB = r.map_BB
+    min_lon, min_lat = BB[0] 
+    max_lon, max_lat = BB[1] 
+    
+    N = 1000
+    #create some random particle positions:
+    lon = random.uniform(min_lon, max_lon, (N,))
+    lat = random.uniform(min_lat, max_lat, (N,))
+
+    #create a spill
+    spill = TestSpillContainer(num_elements=N)
+    spill['positions'][:,0] = lon
+    spill['positions'][:,1] = lat
+
+    r.create_foreground_image()
+    r.draw_elements(spill)
+
+    # create an uncertainty spill
+    lon = random.uniform(min_lon, max_lon, (N,))
+    lat = random.uniform(min_lat, max_lat, (N,))
+
+    spill = TestSpillContainer(num_elements=N, uncertain=True)
+    spill['positions'][:,0] = lon
+    spill['positions'][:,1] = lat
+
+    r.draw_elements(spill)
+
+    # save the image
+    r.save_foreground( os.path.join(output_dir,"foreground1.png") )
+    assert True        
+
+def test_render_beached_elements():
+    """
+    Should this test be in map_cnavas?
+    """
+    r = renderer.Renderer(os.path.join(data_dir, 'MapBounds_2Spillable2Islands2Lakes.bna'),
+                          output_dir,
+                          image_size=(800,600))
+    BB = r.map_BB
+    min_lon, min_lat = BB[0] 
+    max_lon, max_lat = BB[1] 
+    
+    N = 100
+    # create some random particle positions:
+    lon = random.uniform(min_lon, max_lon, (N,))
+    lat = random.uniform(min_lat, max_lat, (N,))
+
+    # create a spill
+    spill = TestSpillContainer(num_elements=N)
+    spill['positions'][:,0] = lon
+    spill['positions'][:,1] = lat
+    # make half of them on land
+    spill['status_codes'][::2]   = basic_types.oil_status.on_land
+
+    r.create_foreground_image()
+    r.draw_elements(spill)
+
+#    assert False
+
+    # create an uncertainty spill
+    lon = random.uniform(min_lon, max_lon, (N,))
+    lat = random.uniform(min_lat, max_lat, (N,))
+
+    spill = TestSpillContainer(num_elements=N, uncertain=True)
+    spill['positions'][:,0] = lon
+    spill['positions'][:,1] = lat
+    # make half of them on land
+    spill['status_codes'][::2]   = basic_types.oil_status.on_land
+
+    r.draw_elements(spill)
+
+    # save the image
+    r.save_foreground( os.path.join(output_dir,"foreground2.png") )
+    assert True        
 
 
+if __name__ == "__main__":
+    pass
 
 
 
