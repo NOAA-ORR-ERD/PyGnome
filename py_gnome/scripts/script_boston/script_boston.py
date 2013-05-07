cba17ad2dcb331121b8188945b14e3dee33fea27
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2013-05-07 12:56:02 -0700

    updated scripts to output all data in netcdf format

--- py_gnome/scripts/script_boston/script_boston.py
+++ py_gnome/scripts/script_boston/script_boston.py
@@ -18,6 +18,7 @@ from gnome.environment import Wind, Tide
 from gnome.utilities import map_canvas
 from gnome.utilities.file_tools import haz_files
 from gnome.persist import scenario
+import scripts
 
 # define base directory
 base_dir = os.path.dirname(__file__)
@@ -47,6 +48,10 @@ def make_model(images_dir=os.path.join(base_dir,"images")):
     
     print "adding outputters"
     model.outputters += renderer
+    
+    netcdf_file = os.path.join(base_dir,'script_boston.nc')
+    scripts.remove_netcdf(netcdf_file)
+    model.outputters += gnome.netcdf_outputter.NetCDFOutput(netcdf_file, all_data=True)
     
     print  "adding a RandomMover:"
     model.movers += gnome.movers.RandomMover(diffusion_coef=100000)
