69f7278400df9ebaf29589aaf545a5d44b680d04
James Makela <james.makela@noaa.gov>
2013-05-30 10:27:19 -0700

    Added Eclipse projects for lib_gnome, lib_adios, py_gnome, and oil_library.

---
+++ project_files/eclipse/AdiosLib/Debug/lib_adios/subdir.mk
@@ -0,0 +1,244 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+/Users/james.makela/Dev/GNOME/lib_adios/ComputeSource.cpp \
+/Users/james.makela/Dev/GNOME/lib_adios/Core.cpp \
+/Users/james.makela/Dev/GNOME/lib_adios/DAGTri.cpp \
+/Users/james.makela/Dev/GNOME/lib_adios/DataConversion.cpp \
+/Users/james.makela/Dev/GNOME/lib_adios/DataExchange.cpp \
+/Users/james.makela/Dev/GNOME/lib_adios/Delaunay.cpp \
+/Users/james.makela/Dev/GNOME/lib_adios/Disperse.cpp \
+/Users/james.makela/Dev/GNOME/lib_adios/EvapExpA.cpp \
+/Users/james.makela/Dev/GNOME/lib_adios/Initialize.cpp \
+/Users/james.makela/Dev/GNOME/lib_adios/InputData.cpp \
+/Users/james.makela/Dev/GNOME/lib_adios/LeakTankB.cpp \
+/Users/james.makela/Dev/GNOME/lib_adios/Maintest.cpp \
+/Users/james.makela/Dev/GNOME/lib_adios/OilInitialize.cpp \
+/Users/james.makela/Dev/GNOME/lib_adios/Pseudo.cpp \
+/Users/james.makela/Dev/GNOME/lib_adios/Response.cpp \
+/Users/james.makela/Dev/GNOME/lib_adios/Smokeplume.cpp \
+/Users/james.makela/Dev/GNOME/lib_adios/SourceToRelease.cpp \
+/Users/james.makela/Dev/GNOME/lib_adios/Spread.cpp \
+/Users/james.makela/Dev/GNOME/lib_adios/TatSnippet.cpp \
+/Users/james.makela/Dev/GNOME/lib_adios/Wave_Energy.cpp \
+/Users/james.makela/Dev/GNOME/lib_adios/blackboard.cpp \
+/Users/james.makela/Dev/GNOME/lib_adios/spreadSimple.cpp \
+/Users/james.makela/Dev/GNOME/lib_adios/xCalcLEArea.cpp 
+
+OBJS += \
+./lib_adios/ComputeSource.o \
+./lib_adios/Core.o \
+./lib_adios/DAGTri.o \
+./lib_adios/DataConversion.o \
+./lib_adios/DataExchange.o \
+./lib_adios/Delaunay.o \
+./lib_adios/Disperse.o \
+./lib_adios/EvapExpA.o \
+./lib_adios/Initialize.o \
+./lib_adios/InputData.o \
+./lib_adios/LeakTankB.o \
+./lib_adios/Maintest.o \
+./lib_adios/OilInitialize.o \
+./lib_adios/Pseudo.o \
+./lib_adios/Response.o \
+./lib_adios/Smokeplume.o \
+./lib_adios/SourceToRelease.o \
+./lib_adios/Spread.o \
+./lib_adios/TatSnippet.o \
+./lib_adios/Wave_Energy.o \
+./lib_adios/blackboard.o \
+./lib_adios/spreadSimple.o \
+./lib_adios/xCalcLEArea.o 
+
+CPP_DEPS += \
+./lib_adios/ComputeSource.d \
+./lib_adios/Core.d \
+./lib_adios/DAGTri.d \
+./lib_adios/DataConversion.d \
+./lib_adios/DataExchange.d \
+./lib_adios/Delaunay.d \
+./lib_adios/Disperse.d \
+./lib_adios/EvapExpA.d \
+./lib_adios/Initialize.d \
+./lib_adios/InputData.d \
+./lib_adios/LeakTankB.d \
+./lib_adios/Maintest.d \
+./lib_adios/OilInitialize.d \
+./lib_adios/Pseudo.d \
+./lib_adios/Response.d \
+./lib_adios/Smokeplume.d \
+./lib_adios/SourceToRelease.d \
+./lib_adios/Spread.d \
+./lib_adios/TatSnippet.d \
+./lib_adios/Wave_Energy.d \
+./lib_adios/blackboard.d \
+./lib_adios/spreadSimple.d \
+./lib_adios/xCalcLEArea.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+lib_adios/ComputeSource.o: /Users/james.makela/Dev/GNOME/lib_adios/ComputeSource.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib_adios/Core.o: /Users/james.makela/Dev/GNOME/lib_adios/Core.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib_adios/DAGTri.o: /Users/james.makela/Dev/GNOME/lib_adios/DAGTri.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib_adios/DataConversion.o: /Users/james.makela/Dev/GNOME/lib_adios/DataConversion.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib_adios/DataExchange.o: /Users/james.makela/Dev/GNOME/lib_adios/DataExchange.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib_adios/Delaunay.o: /Users/james.makela/Dev/GNOME/lib_adios/Delaunay.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib_adios/Disperse.o: /Users/james.makela/Dev/GNOME/lib_adios/Disperse.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib_adios/EvapExpA.o: /Users/james.makela/Dev/GNOME/lib_adios/EvapExpA.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib_adios/Initialize.o: /Users/james.makela/Dev/GNOME/lib_adios/Initialize.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib_adios/InputData.o: /Users/james.makela/Dev/GNOME/lib_adios/InputData.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib_adios/LeakTankB.o: /Users/james.makela/Dev/GNOME/lib_adios/LeakTankB.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib_adios/Maintest.o: /Users/james.makela/Dev/GNOME/lib_adios/Maintest.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib_adios/OilInitialize.o: /Users/james.makela/Dev/GNOME/lib_adios/OilInitialize.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib_adios/Pseudo.o: /Users/james.makela/Dev/GNOME/lib_adios/Pseudo.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib_adios/Response.o: /Users/james.makela/Dev/GNOME/lib_adios/Response.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib_adios/Smokeplume.o: /Users/james.makela/Dev/GNOME/lib_adios/Smokeplume.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib_adios/SourceToRelease.o: /Users/james.makela/Dev/GNOME/lib_adios/SourceToRelease.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib_adios/Spread.o: /Users/james.makela/Dev/GNOME/lib_adios/Spread.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib_adios/TatSnippet.o: /Users/james.makela/Dev/GNOME/lib_adios/TatSnippet.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib_adios/Wave_Energy.o: /Users/james.makela/Dev/GNOME/lib_adios/Wave_Energy.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib_adios/blackboard.o: /Users/james.makela/Dev/GNOME/lib_adios/blackboard.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib_adios/spreadSimple.o: /Users/james.makela/Dev/GNOME/lib_adios/spreadSimple.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib_adios/xCalcLEArea.o: /Users/james.makela/Dev/GNOME/lib_adios/xCalcLEArea.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
