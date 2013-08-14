2bcb679e3f1542d26778f01da43ed0debc7a58bf
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2013-08-14 14:45:02 -0700

    removed commented code and uncommented the section that checks if pytest is run in multiproc mode and runs database setup only on master proc

--- py_gnome/tests/unit_tests/test_oil_library.py
+++ py_gnome/tests/unit_tests/test_oil_library.py
@@ -23,34 +23,30 @@ from gnome.db.oil_library.models import (
     )
 from gnome.utilities import remote_data
 
-#here = os.path.dirname(__file__)
 data_dir = os.path.join( os.path.dirname(__file__), r'sample_data/oil_library')
 db_file = os.path.join(data_dir, r'OilLibrary.db')  # create this database from OilLib.smaller
-#db_file = os.path.join(here, r'sample_data/oil_library/OilLibrary.db')  # database is generated in conftest.py from the text file OilLib.smaller
 
 def make_db():
-    #from py.test import config
+    from py.test import config
     
     # Only run database setup on master (in case of xdist/multiproc mode)
-    #if not hasattr(config, 'slaveinput'):
-    try:
-        from gnome.db.oil_library.initializedb import initialize_sql, load_database
-    
-        oillib_file = remote_data.get_datafile( os.path.join( data_dir, r'OilLib.smaller') )
-        #db_file = os.path.join(data_dir, r'OilLibrary.db')  # create this database from OilLib.smaller
-        sqlalchemy_url = 'sqlite:///{0}'.format(db_file)
-        settings = {'sqlalchemy.url': sqlalchemy_url,
-                    'oillib.file': oillib_file
-                    }
-        initialize_sql(settings)
-        load_database(settings)
-    except ImportError as ie:
-        print "\nWarning: Required modules for database unit-testing not found."
-        dependant_modules = ('sqlalchemy','zope.sqlalchemy','transaction')
-        print ie
-        print "Also may need:",
-        print '\t {0}\n'.format([m for m in dependant_modules if not m in sys.modules])
-
+    if not hasattr(config, 'slaveinput'):
+        try:
+            from gnome.db.oil_library.initializedb import initialize_sql, load_database
+        
+            oillib_file = remote_data.get_datafile( os.path.join( data_dir, r'OilLib.smaller') )
+            sqlalchemy_url = 'sqlite:///{0}'.format(db_file)
+            settings = {'sqlalchemy.url': sqlalchemy_url,
+                        'oillib.file': oillib_file
+                        }
+            initialize_sql(settings)
+            load_database(settings)
+        except ImportError as ie:
+            print "\nWarning: Required modules for database unit-testing not found."
+            dependant_modules = ('sqlalchemy','zope.sqlalchemy','transaction')
+            print ie
+            print "Also may need:",
+            print '\t {0}\n'.format([m for m in dependant_modules if not m in sys.modules])
 
 make_db()
 sqlalchemy_url = 'sqlite:///%s' % (db_file)
