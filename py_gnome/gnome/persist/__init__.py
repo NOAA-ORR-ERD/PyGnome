4c46a640c5c336481ca2316c3692d03c94513907
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2013-10-29 11:00:17 -0700

    updated spill name PointSourceSurfaceRelease to PointLineSource PEP8 compliance for persist/__init__.py

--- py_gnome/gnome/persist/__init__.py
+++ py_gnome/gnome/persist/__init__.py
@@ -7,7 +7,7 @@ from gnome.persist import monkey_patch_colander
 monkey_patch_colander.apply()
 
 
-""" 
+"""
 Define a dict that maps the py_gnome classes or modules to schema modules.
 The class names of the schema corresponds with the pyGnome class name.
 
@@ -17,14 +17,14 @@ gnome.model.Model has a corresponding schema gnome.persist.model_schema.Model
 This dict just tells us for each pyGnome module or class, where to look for
 corresponding schema class
 """
-modules_dict= {'gnome.environment':'gnome.persist.environment_schema',
-               'gnome.map':'gnome.persist.map_schema',
-               'gnome.model':'gnome.persist.model_schema',
-               'gnome.movers.movers':'gnome.persist.movers_schema',
-               'gnome.movers.random_movers':'gnome.persist.movers_schema',
-               'gnome.movers.wind_movers':'gnome.persist.movers_schema',
-               'gnome.movers.current_movers':'gnome.persist.movers_schema',
-               'gnome.spill':'gnome.persist.spills_schema',
-               'gnome.renderer':'gnome.persist.outputters_schema',
-               'gnome.netcdf_outputter':'gnome.persist.outputters_schema',
-               }
+modules_dict = {'gnome.environment': 'gnome.persist.environment_schema',
+                'gnome.map': 'gnome.persist.map_schema',
+                'gnome.model': 'gnome.persist.model_schema',
+                'gnome.movers.movers': 'gnome.persist.movers_schema',
+                'gnome.movers.random_movers': 'gnome.persist.movers_schema',
+                'gnome.movers.wind_movers': 'gnome.persist.movers_schema',
+                'gnome.movers.current_movers': 'gnome.persist.movers_schema',
+                'gnome.spill': 'gnome.persist.spills_schema',
+                'gnome.renderer': 'gnome.persist.outputters_schema',
+                'gnome.netcdf_outputter': 'gnome.persist.outputters_schema',
+                }
