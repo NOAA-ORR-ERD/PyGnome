cafe6657117fc825ddb2cb6c3d3849e9c76ff06b
Caitlin O'Connor <caitlin.oconnor@noaa.gov>
2013-11-13 09:49:11 -0800

    added uncertainty to chesapeake, added a script using gridded winds (with uncertainty)

--- py_gnome/scripts/script_chesapeake_bay/script_chesapeake_bay.py
+++ py_gnome/scripts/script_chesapeake_bay/script_chesapeake_bay.py
@@ -31,7 +31,7 @@ def make_model(images_dir=os.path.join(base_dir, 'images')):
     start_time = datetime(2004, 12, 31, 13, 0)
     model = gnome.model.Model(start_time=start_time,
                               duration=timedelta(days=1), time_step=30
-                              * 60, uncertain=False)  # 1 day of data in file
+                              * 60, uncertain=True)  # 1 day of data in file
                                                       # 1/2 hr in seconds
 
     mapfile = get_datafile(os.path.join(base_dir, './ChesapeakeBay.bna'
@@ -41,7 +41,7 @@ def make_model(images_dir=os.path.join(base_dir, 'images')):
 
     renderer = gnome.renderer.Renderer(mapfile, images_dir, size=(800,
             600))
-    renderer.viewport = ((-76.5, 37.25), (-75.8, 37.75))
+    renderer.viewport = ((-76.5, 37.), (-75.8, 38.))
 
     print 'adding outputters'
     model.outputters += renderer
@@ -72,7 +72,7 @@ def make_model(images_dir=os.path.join(base_dir, 'images')):
     series[1] = (start_time + timedelta(hours=23), (30, 0))
 
     wind = Wind(timeseries=series, units='knot')
-    w_mover = gnome.movers.WindMover(wind)
+    w_mover = gnome.movers.WindMover(wind, uncertain_angle_scale=0) 	# default is .4 radians
     model.movers += w_mover
 
     print 'adding a current mover:'
@@ -81,7 +81,9 @@ def make_model(images_dir=os.path.join(base_dir, 'images')):
                              r"./ChesapeakeBay.nc"))
     topology_file = get_datafile(os.path.join(base_dir,
                                  r"./ChesapeakeBay.dat"))
-    c_mover = gnome.movers.GridCurrentMover(curr_file, topology_file)
+    c_mover = gnome.movers.GridCurrentMover(curr_file, topology_file, uncertain_time_delay = 3)	# uncertain_time_delay in hours
+    c_mover.uncertain_along = 0		# default is .5
+    #c_mover.uncertain_cross = 0	# default is .25
     model.movers += c_mover
 
     return model
