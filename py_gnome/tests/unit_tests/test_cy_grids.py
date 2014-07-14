177f7ffe3089e38f1aa267fc6bbdb4bba99088d9
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2014-07-14 10:44:48 -0700

    updated grid test

--- py_gnome/tests/unit_tests/test_cy_grids.py
+++ py_gnome/tests/unit_tests/test_cy_grids.py
@@ -4,23 +4,39 @@ test cython wrappers around grid objects
 import os
 from datetime import datetime
 
+import netCDF4 as nc
+import numpy as np
+
 from gnome.cy_gnome.cy_grid_rect import CyTimeGridWindRect
 from gnome.cy_gnome.cy_grid_curv import CyTimeGridWindCurv
 from gnome.utilities.time_utils import date_to_sec
+from gnome.basic_types import velocity_rec
 
-wind_base = 'sample_data/winds/'
+wind_base = os.path.join(os.path.dirname(__file__), 'sample_data/winds/')
 f = os.path.join(wind_base, 'test_wind.cdf')
 f2 = os.path.join(wind_base, 'WindSpeedDirSubset.nc')
 t2 = os.path.join(wind_base, 'WindSpeedDirSubsetTop.dat')
 
 
 def test_grid_wind_rect():
+    '''
+    check CyTimeGridWindRect correctly parses data
+    '''
+    idx = 5
+    with nc.Dataset(f) as data:
+        time = nc.num2date(data.variables['time'][idx],
+                           units=data.variables['time'].units)
+        long_lat = (data.variables['lon'][idx],
+                    data.variables['lat'][idx])
+        exp_vel = (data.variables['air_u'][idx][idx][idx],
+                   data.variables['air_v'][idx][idx][idx])
+
     c = CyTimeGridWindRect(f)
-    time = date_to_sec(datetime(1999, 11, 29, 21))
-    vel = c.get_value(time, (3.104588, 52.016468))
+    time = date_to_sec(time)
+    vel = c.get_value(time, long_lat)
     print "\nRect grid - vel: {0}".format(vel)
-    for key in vel:
-        assert vel[key] != 0
+    print "Rect grid - expected_vel: {0}".format(exp_vel)
+    assert (vel.item() == exp_vel)
 
 
 def test_grid_wind_curv():
