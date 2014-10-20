2e9227352c3a9e364cf2892aa89f17dff75312c7
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2014-10-20 15:23:30 -0700

    added test to check that sum of all masses in weathering_data at end of timestep equals the initail mass at beginning of step. This is true for 'nominal' output. For fake 'high', 'low', we're sampling from uniform distribution to add uncertaintly after the step so this check doesn't work

--- py_gnome/tests/unit_tests/test_outputters/test_weathering_outputter.py
+++ py_gnome/tests/unit_tests/test_outputters/test_weathering_outputter.py
@@ -10,13 +10,14 @@ import pytest
 import geojson
 
 from gnome.environment import constant_wind, Water
-from gnome.weatherers import Evaporation
+from gnome.weatherers import Evaporation, Dispersion, Skimmer, Burn
 from gnome.spill.elements import floating_weathering
 
-from gnome.spill import SpatialRelease, Spill, point_line_release_spill
+#from gnome.spill import SpatialRelease, Spill, point_line_release_spill
+from gnome.spill import point_line_release_spill
 
 from gnome.outputters import WeatheringOutput
-from gnome.persist import load
+#from gnome.persist import load
 
 here = os.path.dirname(__file__)
 
@@ -41,8 +42,11 @@ def model(sample_model):
     model.environment += [Water(311.15),
                           constant_wind(1.0, 0.0)]
     # figure out mid-run save for weathering_data attribute, then add this in
-    model.weatherers += Evaporation(model.environment[-2],
-                                    model.environment[-1])
+    model.weatherers += [Evaporation(model.environment[-2],
+                                    model.environment[-1]),
+                         Dispersion(),
+                         Burn(),
+                         Skimmer()]
 
     N = 10  # a line of ten points
     line_pos = np.zeros((N, 3), dtype=np.float64)
@@ -78,14 +82,36 @@ def test_init():
 def test_model_webapi_output(model):
     'Test weathering outputter with a model since simplest to do that'
     model.rewind()
+
+    # floating mass at beginning of step - though tests will only pass for
+    # nominal values
+    step_init_float = {'nominal': 0.0, 'high': 0.0, 'low': 0.0}
     for step in model:
         assert 'WeatheringOutput' in step
         for key in ('nominal', 'low', 'high'):
-            assert ('evaporated' in
-                    step['WeatheringOutput'][key])
-        print step['WeatheringOutput']['nominal']
-        #if step['step_num'] == 0:
-        #    assert 'weathering_data' not in step
+            sum_mass = 0.0
+            for process in ('evaporated', 'burned', 'skimmed', 'dispersed'):
+                assert (process in step['WeatheringOutput'][key])
+                sum_mass += step['WeatheringOutput'][key][process]
+
+            assert (step['WeatheringOutput'][key]['floating'] <=
+                    step['WeatheringOutput'][key]['amount_released'])
+            # For nominal, sum up all mass and ensure it equals the mass at
+            # step initialization - ignore step 0
+            sum_mass += step['WeatheringOutput'][key]['floating']
+            if key == 'nominal' and step_init_float[key] > 0.0:
+                'assert after 1st timestep'
+                np.isclose(sum_mass, step_init_float[key])
+
+                print '\n{0} values: '.format(key)
+                #print 'released: {0}'.format(step['WeatheringOutput'][key]['amount_released'])
+                print ('amount before weathering at step beginning: {0}'.
+                       format(step_init_float[key]))
+                print 'weathered + floating at step end: {0}'.format(sum_mass)
+
+            # update initial mass at beginning of next step - used for
+            # assertion
+            step_init_float[key] = step['WeatheringOutput'][key]['floating']
 
 
 def test_model_dump_output(model):
