c65a551bbf542b23c319e86f8b9b7c99b1e3a436
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2014-11-15 22:29:50 -0800

    added conftest.py

---
+++ py_gnome/tests/unit_tests/test_outputters/conftest.py
@@ -0,0 +1,23 @@
+'''
+common fixture for output_dirs required by different outputters
+'''
+import os
+import shutil
+
+import pytest
+
+
+@pytest.fixture(scope='module')
+def output_dir(dump, request):
+    '''
+    return a default output directory for outputter to dump its data
+    construct an output directory name from request object
+    write it as module scope since the outputter should rewind and clean out
+    the directory before running model
+    '''
+    d_name = request.fspath.purebasename.split('_')[1] + '_outputdir'
+    odir = os.path.join(dump, d_name)
+    if os.path.isdir(odir):
+        shutil.rmtree(odir)
+    os.mkdir(odir)
+    return odir
