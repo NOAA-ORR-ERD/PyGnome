2fb5a29705c104c62123f04e6e1f10982e732b25
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2014-12-24 21:06:37 -0800

    updated cleanup classes to use itersubstancedata() so copied data stays in sync

--- py_gnome/gnome/weatherers/cleanup.py
+++ py_gnome/gnome/weatherers/cleanup.py
@@ -155,14 +155,17 @@ class Burn(Weatherer, Serializable):
 
     def weather_elements(self, sc, time_step, model_time):
         'for now just take away 0.1% at every step'
-        if self.active:
-            # take out 0.25% of the mass
-            pct_per_le = (1 - 0.25/sc['mass_components'].shape[1])
-            mass_remain = pct_per_le * sc['mass_components']
-            sc.weathering_data['burned'] += \
-                np.sum(sc['mass_components'][:, :] - mass_remain[:, :])
-            sc['mass_components'] = mass_remain
-            sc['mass'][:] = sc['mass_components'].sum(1)
+        if self.active and len(sc) > 0:
+            for substance, data in sc.itersubstancedata(self._arrays):
+                # take out 0.25% of the mass
+                pct_per_le = (1 - 0.25/data['mass_components'].shape[1])
+                mass_remain = pct_per_le * data['mass_components']
+                sc.weathering_data['burned'] += \
+                    np.sum(data['mass_components'][:, :] - mass_remain[:, :])
+                data['mass_components'] = mass_remain
+                data['mass'][:] = data['mass_components'].sum(1)
+
+        sc.update_from_substancedata(self._arrays)
 
 
 class Dispersion(Weatherer, Serializable):
@@ -175,11 +178,14 @@ class Dispersion(Weatherer, Serializable):
 
     def weather_elements(self, sc, time_step, model_time):
         'for now just take away 0.1% at every step'
-        if self.active:
-            # take out 0.015% of the mass
-            pct_per_le = (1 - 0.015/sc['mass_components'].shape[1])
-            mass_remain = pct_per_le * sc['mass_components']
-            sc.weathering_data['dispersed'] += \
-                np.sum(sc['mass_components'][:, :] - mass_remain[:, :])
-            sc['mass_components'] = mass_remain
-            sc['mass'][:] = sc['mass_components'].sum(1)
+        if self.active and len(sc) > 0:
+            for substance, data in sc.itersubstancedata(self._arrays):
+                # take out 0.25% of the mass
+                pct_per_le = (1 - 0.015/data['mass_components'].shape[1])
+                mass_remain = pct_per_le * data['mass_components']
+                sc.weathering_data['dispersed'] += \
+                    np.sum(data['mass_components'][:, :] - mass_remain[:, :])
+                data['mass_components'] = mass_remain
+                data['mass'][:] = data['mass_components'].sum(1)
+
+        sc.update_from_substancedata(self._arrays)
