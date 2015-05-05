9d59f88ac9202d927e6a0aa6a60cbb378026cb03
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2015-05-05 13:29:31 -0700

    round timeseries when serializing data

--- py_gnome/gnome/environment/wind.py
+++ py_gnome/gnome/environment/wind.py
@@ -218,6 +218,14 @@ class Wind(serializable.Serializable, Timeseries, Environment):
         '''
         self.set_wind_data(value, units=self.units)
 
+    def timeseries_to_dict(self):
+        '''
+        when serializing data - round it to 2 decimal places
+        '''
+        ts = self.get_wind_data(units=self.units)
+        ts['value'][:] = np.round(ts['value'], 2)
+        return ts
+
     @property
     def units(self):
         '''
