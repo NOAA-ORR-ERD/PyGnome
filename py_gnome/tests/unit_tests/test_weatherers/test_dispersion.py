1bc126ae7c57bafc4bd9ac9734d2fca8c182e6f6
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2015-05-14 02:56:27 -0700

    fixed test to invoke prepare_for_model_run() before updating

--- py_gnome/tests/unit_tests/test_weatherers/test_dispersion.py
+++ py_gnome/tests/unit_tests/test_weatherers/test_dispersion.py
@@ -45,6 +45,7 @@ def test_dispersion(oil, temp, num_elems, on):
                             arr_types=arrays)
      sc.amount = 10000
      time_step = 15. * 60
+     intrinsic.prepare_for_model_run(sc)
      intrinsic.update(sc.num_released, sc)
      model_time = (sc.spills[0].get('release_time') +
                    timedelta(seconds=time_step))
@@ -84,6 +85,7 @@ def test_dispersion_not_active(oil, temp, num_elems):
                             arr_types=arrays)
      sc.amount = 10000
      time_step = 15. * 60
+     intrinsic.prepare_for_model_run(sc)
      intrinsic.update(sc.num_released, sc)
      model_time = (sc.spills[0].get('release_time') +
                    timedelta(seconds=time_step))
