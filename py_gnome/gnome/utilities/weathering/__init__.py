0c604935d47c64de3fcf645cd7aa2922d8688b24
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2015-05-22 12:53:18 -0700

    deleted unused/experimental weathering module

--- py_gnome/gnome/utilities/weathering/__init__.py
+++
@@ -1,5 +0,0 @@
-from weathering import (WeatheringComponent,
-                        weather_curve,
-                        OilTypes)
-
-__all__ = [WeatheringComponent, weather_curve, OilTypes, 'graphs']
