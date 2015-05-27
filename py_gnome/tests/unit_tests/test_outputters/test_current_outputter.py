3b69317796c552999e2ff3b1b2519d1383d33647
James L. Makela <james.makela@noaa.gov>
2015-05-26 17:57:17 -0700

    Trying out a new strategy for the current outputter.  We will quantize our velocities and output the matching multi points for them.

--- py_gnome/tests/unit_tests/test_outputters/test_current_outputter.py
+++ py_gnome/tests/unit_tests/test_outputters/test_current_outputter.py
@@ -111,28 +111,7 @@ def test_current_grid_geojson_output(model):
 
                 geometry = feature['geometry']
                 assert 'type' in geometry
-                assert geometry['type'] == 'Point'
+                assert geometry['type'] == 'MultiPoint'
 
                 assert 'coordinates' in geometry
-                assert len(geometry['coordinates']) == 2
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                assert len(geometry['coordinates']) > 0
