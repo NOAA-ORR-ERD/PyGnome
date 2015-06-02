599020959fba10cecce7fd042b3b90c2f431a74d
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2015-06-02 12:27:15 -0700

    added exceptions module

---
+++ py_gnome/gnome/exceptions.py
@@ -0,0 +1,19 @@
+'''
+PyGnome custom exceptions
+'''
+
+
+class GnomeRuntimeError(Exception):
+    def __init__(self, *args):
+        '''
+        *args can contains a message, and other arguments
+        '''
+        super(GnomeRuntimeError, self).__init__(*args)
+
+
+class ReferencedObjectNotSet(Exception):
+    '''
+    *args can contains a message, and other arguments
+    '''
+    def __init__(self, *args):
+        super(GnomeRuntimeError, self).__init__(*args)
