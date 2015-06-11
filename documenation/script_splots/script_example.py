7bf2c7f92e1c94d3c80f27b514ae3eb94fc7a36c
Jasmine Sandhu <jasmine.sandhu@noaa.gov>
2015-06-11 01:03:52 -0700

    added example script_splots and update README

---
+++ documenation/script_splots/script_example.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+"""
+Example script to set release from splots, includes diffusion and wind mover
+"""
+
+import os
+from datetime import datetime, timedelta
+
+import numpy as np
+
+from gnome import scripting, initialize_log
+from gnome.basic_types import datetime_value_2d
+
+from gnome.utilities.projections import GeoProjection
+
+from gnome.model import Model
+from gnome.environment import Wind
+from gnome.movers import RandomMover, WindMover
+from gnome.spill import Spill
+from gnome.spill.release import release_from_splot_data
+from gnome.map import MapFromBNA    # will be used once we have BNA map
+
+from gnome.outputters import Renderer
+from gnome.outputters import NetCDFOutput
+
+# define base directory
+base_dir = os.path.dirname(__file__)
+
+
+def make_model(images_dir):
+    print 'initializing the model'
+
+    timestep = timedelta(minutes=15)    # this is already default
+    start_time = datetime(2012, 9, 15, 12, 0)
+    model = Model(timestep, start_time)
+
+    # timeseries for wind data. The value is interpolated if time is between
+    # the given datapoints
+    series = np.zeros((4, ), dtype=datetime_value_2d)
+    series[:] = [(start_time, (5, 180)),
+                 (start_time + timedelta(hours=6), (10, 180)),
+                 (start_time + timedelta(hours=12), (12, 180)),
+                 (start_time + timedelta(hours=18), (8, 180))]
+    wind = Wind(timeseries=series, units='m/s')
+    model.environment += wind
+
+    # include a wind mover and random diffusion
+    print 'adding movers'
+    model.movers += [WindMover(wind), RandomMover()]
+
+    # add particles
+    print 'adding particles'
+    release = release_from_splot_data(start_time,
+                                      'GL.2013267._LE_WHOLELAKE.txt')
+    model.spills += Spill(release)
+
+    # output data as png images and in netcdf format
+    print 'adding outputters'
+    netcdf_file = os.path.join(base_dir, 'script_example.nc')
+
+    # ignore renderer for now
+    model.outputters += [Renderer(images_dir=images_dir, size=(800, 800),
+                                  projection_class=GeoProjection),
+                         NetCDFOutput(netcdf_file)]
+
+    print 'model complete'
+    return model
+
+
+if __name__ == '__main__':
+    initialize_log(os.path.join(base_dir, 'log_config.json'))
+    images_dir = os.path.join(base_dir, 'images')
+    scripting.make_images_dir(images_dir)
+    model = make_model(images_dir)
+    model.full_run()
