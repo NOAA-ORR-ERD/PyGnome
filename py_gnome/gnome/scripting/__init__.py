ef7eb77aea9f1b1788c407ffbd35ede0c208ebb9
amy.macfadyen <amy.macfadyen@gmail.com>
2015-09-29 16:41:55 -0700

    Continued adding content to Sphinx docs (scripting interface) Added import of helper functions to gnome.scripting Raised error if "substance" is specified both as a Spill attribute and in ElementType (the ElementType substance would override but better to let the user know they are doing something strange)

--- py_gnome/gnome/scripting/__init__.py
+++ py_gnome/gnome/scripting/__init__.py
@@ -5,3 +5,7 @@ assorted utilities that make it easier to write scripts to automate gnome
 
 """
 from .utilities import *
+from gnome.environment.wind import constant_wind
+from gnome.spill import point_line_release_spill
+from gnome.movers.wind import constant_wind_mover
+from gnome.movers.wind import wind_mover_from_file
