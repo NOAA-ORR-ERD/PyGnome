b82e4c9ecb60ee473829be45f6b7788eca829c68
jay.hennen <jay.hennen@noaa.gov>
2015-10-13 13:03:07 -0700

    added parameterized shore script

---
+++ py_gnome/scripts/script_shore_param/script_shore_param.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+"""
+Script to test GNOME with HYCOM data in Mariana Islands region.
+"""
+
+import os
+from datetime import datetime, timedelta
+
+from gnome import basic_types
+
+from gnome import scripting
+from gnome import utilities
+from gnome.utilities.remote_data import get_datafile
+
+from gnome.model import Model
+
+from gnome.map import Param_Map
+from gnome.spill import point_line_release_spill
+from gnome.movers import RandomMover, constant_wind_mover, GridCurrentMover
+
+from gnome.outputters import (Renderer,
+                              # NetCDFOutput
+                              )
+from gnome.basic_types import numerical_methods
+
+NUM_ELEMENTS = 1e6
+
+# define base directory
+base_dir = os.path.dirname(__file__)
+
+
+def make_model(img_dir=os.path.join(base_dir, 'images')):
+    print 'initializing the model'
+
+    start_time = datetime(2013, 5, 18, 0)
+
+    model = Model(start_time=start_time, duration=timedelta(days=8),
+                  time_step=3600, uncertain=False)
+
+    mapfile = get_datafile(os.path.join(base_dir, 'mariana_island.bna'))
+
+    print 'adding the map'
+    p_map = model.map = Param_Map(center = (145.25,15.0), distance=10000, bearing = 90 )  # hours
+
+    #
+    # Add the outputters -- render to images, and save out as netCDF
+    #
+
+    print 'adding renderer'
+    model.outputters += Renderer( images_dir=img_dir,
+                                 size=(800, 600),
+                                 map_BB=p_map.get_map_bounds(),
+                                 land_polygons=p_map.get_land_polygon()
+                                 )
+#                                 draw_back_to_fore=True)
+
+    # print "adding netcdf output"
+    # netcdf_output_file = os.path.join(base_dir,'mariana_output.nc')
+    # scripting.remove_netcdf(netcdf_output_file)
+    # model.outputters += NetCDFOutput(netcdf_output_file, which_data='all')
+
+    #
+    # Set up the movers:
+    #
+
+    print 'adding a RandomMover:'
+    model.movers += RandomMover(diffusion_coef=10000)
+
+    print 'adding a simple wind mover:'
+    model.movers += constant_wind_mover(5, 315, units='m/s')
+
+    print 'adding a current mover:'
+
+    # # this is HYCOM currents
+    curr_file = get_datafile(os.path.join(base_dir, 'HYCOM.nc'))
+    model.movers += GridCurrentMover(curr_file,
+                                     num_method=numerical_methods.euler);
+
+    # #
+    # # Add some spills (sources of elements)
+    # #
+
+    print 'adding four spill'
+    model.spills += point_line_release_spill(num_elements=NUM_ELEMENTS // 4,
+                                             start_position=(145.25, 15.0,
+                                                             0.0),
+                                             release_time=start_time)
+
+    return model
+
+
+if __name__ == '__main__':
+    scripting.make_images_dir()
+    model = make_model()
+    for step in model:
+        print "step: %.4i -- memuse: %fMB" % (step['step_num'],
+                                              utilities.get_mem_use())
+    # model.full_run(log=True)
