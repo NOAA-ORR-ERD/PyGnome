fixed rotation in vector field

py_gnome/gnome/environment/vector_field.py
@@ -436,8 +436,6 @@ class SField(VectorField):
         ind = indices
         t_alphas = self.time.interp_alpha(time)
         t_index = self.time.indexof(time)
-        if ind is None:
-            ind = self.grid.locate_faces(points, memo=mem, copy=False)
 
         s1 = [t_index]
         s2 = [t_index + 1]
@@ -448,29 +446,27 @@ class SField(VectorField):
             s2.append(depth)
             s3.append(depth)
             s4.append(depth)
+            
+        sg = False
 
-        u0 = self.grid.interpolate_var_to_points(points, self.u, slices=s1, slice_grid=False, memo=mem)
-        u1 = self.grid.interpolate_var_to_points(points, self.u, slices=s2, slice_grid=False, memo=mem)
+        u0 = self.grid.interpolate_var_to_points(points, self.u, slices=s1, slice_grid=sg, memo=mem)
+        u1 = self.grid.interpolate_var_to_points(points, self.u, slices=s2, slice_grid=sg, memo=mem)
 
-        v0 = self.grid.interpolate_var_to_points(points, self.v, slices=s3, slice_grid=False, memo=mem)
-        v1 = self.grid.interpolate_var_to_points(points, self.v, slices=s4, slice_grid=False, memo=mem)
+        v0 = self.grid.interpolate_var_to_points(points, self.v, slices=s3, slice_grid=sg, memo=mem)
+        v1 = self.grid.interpolate_var_to_points(points, self.v, slices=s4, slice_grid=sg, memo=mem)
 
         u_vels = u0 + (u1 - u0) * t_alphas
         v_vels = v0 + (v1 - v0) * t_alphas
 
-
-        vels = np.ma.column_stack((u_vels, v_vels))
         if self.grid.angles is not None:
             angs = self.grid.interpolate_var_to_points(points, self.grid.angles, slices=None, slice_grid=False, memo=mem)
-#             ang_ind = ind + [1, 1]
-#             angs = self.grid.angles[:][ang_ind[:, 0], ang_ind[:, 1]]
-            u_vels = u_vels*np.cos(angs) - v_vels*np.sin(angs)
-            v_vels = u_vels*np.sin(angs) + v_vels*np.cos(angs)
+            u_rot = u_vels*np.cos(angs) - v_vels*np.sin(angs)
+            v_rot = u_vels*np.sin(angs) + v_vels*np.cos(angs)
 #             rotations = np.array(
 #                 ([np.cos(angs), -np.sin(angs)], [np.sin(angs), np.cos(angs)]))
 
 #             return np.matmul(rotations.T, vels[:, :, np.newaxis]).reshape(-1, 2)
-        vels = np.ma.column_stack((u_vels, v_vels))
+        vels = np.ma.column_stack((u_rot, v_rot))
         return vels
 
     def get_edges(self, bounds=None):