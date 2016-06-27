fixed wind_mover numerical methods

py_gnome/gnome/movers/py_wind_movers.py
@@ -24,10 +24,14 @@ class PyWindMover(movers.Mover, serializable.Serializable):
 
     def __init__(self,
                  wind=None,
+                 filename=None,
+                 extrapolate=False,
+                 time_offset=0,
                  uncertain_duration=3,
                  uncertain_time_delay=0,
                  uncertain_speed_scale=2.,
                  uncertain_angle_scale=0.4,
+                 default_num_method='Trapezoid',
                  **kwargs):
         """
         Uses super to call CyMover base class __init__
@@ -49,6 +53,8 @@ class PyWindMover(movers.Mover, serializable.Serializable):
             kwargs['name'] = \
                 kwargs.pop('name', wind.name)
 
+        self.filename=filename
+        self.extrapolate=extrapolate
         self.uncertain_duration = uncertain_duration
         self.uncertain_time_delay = uncertain_time_delay
         self.uncertain_speed_scale = uncertain_speed_scale
@@ -60,8 +66,34 @@ class PyWindMover(movers.Mover, serializable.Serializable):
                                  'windage_range',
                                  'windage_persist'})
 
+        self.num_methods = {'RK4': self.get_delta_RK4,
+                            'Euler': self.get_delta_Euler,
+                            'Trapezoid':self.get_delta_Trapezoid}
+        self.default_num_method=default_num_method
+
 
         # set optional attributes
+
+    @classmethod
+    def from_netCDF(cls,
+                    filename=None,
+                    extrapolate=False,
+                    time_offset=0,
+                    current_scale=1,
+                    uncertain_duration=24 * 3600,
+                    uncertain_time_delay=0,
+                    uncertain_along=.5,
+                    uncertain_across=.25,
+                    uncertain_cross=.25):
+        wind = GridWind.from_netCDF(filename)
+        return cls(wind=wind,
+                   filename=filename,
+                   extrapolate=extrapolate,
+                   time_offset=time_offset,
+                   current_scale=current_scale,
+                   uncertain_along=uncertain_along,
+                   uncertain_across=uncertain_across,
+                   uncertain_cross=uncertain_cross)
 
     @property
     def wind(self):
@@ -94,7 +126,7 @@ class PyWindMover(movers.Mover, serializable.Serializable):
                                      sc['windage_persist'],
                                      time_step)
 
-    def get_move(self, sc, time_step, model_time_datetime):
+    def get_move(self, sc, time_step, model_time_datetime, num_method = None):
         """
         Compute the move in (long,lat,z) space. It returns the delta move
         for each element of the spill as a numpy array of size
@@ -111,17 +143,47 @@ class PyWindMover(movers.Mover, serializable.Serializable):
 
         All movers must implement get_move() since that's what the model calls
         """
+        method = None
+        if num_method is None:
+            method = self.num_methods[self.default_num_method]
+        else:
+            method = self.num_method[num_method]
+
         status = sc['status_codes'] != oil_status.in_water
         positions = sc['positions']
-
-        vels = self.wind.at(positions[:, 0:2], model_time_datetime, units='m/s')
-        vels[:,0] *= sc['windages']
-        vels[:,1] *= sc['windages']
-
         deltas = np.zeros_like(positions)
-        deltas[:] = 0.
-        deltas[:, 0:2] = vels * time_step
+        pos = positions[:, 0:2]
+
+        deltas[:, 0:2] = method(sc, time_step, model_time_datetime, pos)
+        deltas[:,0] *= sc['windages']
+        deltas[:,1] *= sc['windages']
+
         deltas = FlatEarthProjection.meters_to_lonlat(deltas, positions)
         deltas[status] = (0, 0, 0)
-        pass
         return deltas
+
+    def get_delta_Euler(self, sc, time_step, model_time, pos):
+        vels = self.wind.at(pos[:, 0:2], model_time, extrapolate=self.extrapolate)
+        return vels * time_step
+
+    def get_delta_Trapezoid(self, sc, time_step, model_time, pos):
+        dt = datetime.timedelta(seconds=time_step)
+        dt_s = dt.seconds
+        t = model_time
+        v0 = self.wind.at(pos, t, extrapolate=self.extrapolate).data
+        d0 = FlatEarthProjection.meters_to_lonlat(v0 * dt_s, pos)
+        v1 = self.wind.at(pos + d0, t + dt, extrapolate=self.extrapolate).data
+        return  dt_s/2 * (v0 + v1)
+
+    def get_delta_RK4(self, sc, time_step, model_time, pos):
+        dt = datetime.timedelta(seconds=time_step)
+        dt_s = dt.seconds
+        t = model_time
+        v0 = self.wind.at(pos, t, extrapolate=self.extrapolate).data
+        d0 = FlatEarthProjection.meters_to_lonlat(v0 * dt_s/2, pos)
+        v1 = self.wind.at(pos + d0, t + dt/2, extrapolate=self.extrapolate).data
+        d1 = FlatEarthProjection.meters_to_lonlat(v1 * dt_s/2, pos)
+        v2 = self.wind.at(pos + d1, t + dt/2, extrapolate=self.extrapolate).data
+        d2 = FlatEarthProjection.meters_to_lonlat(v2 * dt_s, pos)
+        v3 = self.wind.at(pos + d2, t + dt, extrapolate=self.extrapolate).data
+        return dt_s/6 * (v0 + 2*v1 + 2*v2 + v3)