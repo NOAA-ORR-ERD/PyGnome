33d6cd6babbbdb6474c0f28160625c2cd13abe34
Chris Barker <Chris.Barker@noaa.gov>
2016-08-10 14:20:49 -0700

    updated script_ice to auto-download files, but it still fails

--- py_gnome/scripts/script_ice/script_ice.py
+++ py_gnome/scripts/script_ice/script_ice.py
@@ -1,6 +1,8 @@
 """
-Script to test GNOME with chesapeake bay data (netCDF 3D triangle grid)
-Eventually update to use Grid Map rather than BNA
+Example of using gnome in "ice infested waters"
+
+With the ice and current data coming from a ROMS coupled ocean-ice model.
+
 """
 
 import os
@@ -90,25 +92,25 @@ def make_model(images_dir=os.path.join(base_dir, 'images')):
 
     print 'adding a current mover:'
 
-    fn = ['N:\\Users\\Dylan.Righi\\OutBox\\ArcticROMS\\arctic_avg2_0001_gnome.nc',
-                 'N:\\Users\\Dylan.Righi\\OutBox\\ArcticROMS\\arctic_avg2_0002_gnome.nc']
+    fn = [get_datafile('arctic_avg2_0001_gnome.nc'),
+          get_datafile('arctic_avg2_0001_gnome.nc'),
+          ]
 
-    gt = {'node_lon':'lon',
-          'node_lat':'lat'}
-#     fn='arctic_avg2_0001_gnome.nc'
+    gt = {'node_lon': 'lon',
+          'node_lat': 'lat'}
 
     ice_aware_curr = IceAwareCurrent.from_netCDF(filename=fn,
                                                  grid_topology=gt)
     ice_aware_wind = IceAwareWind.from_netCDF(filename=fn,
-                                              grid = ice_aware_curr.grid,)
+                                              grid=ice_aware_curr.grid,)
     method = 'Trapezoid'
 
 #     i_c_mover = PyGridCurrentMover(current=ice_aware_curr)
 #     i_c_mover = PyGridCurrentMover(current=ice_aware_curr, default_num_method='Euler')
     i_c_mover = PyGridCurrentMover(current=ice_aware_curr, default_num_method=method)
-    i_w_mover = PyWindMover(wind = ice_aware_wind, default_num_method=method)
+    i_w_mover = PyWindMover(wind=ice_aware_wind, default_num_method=method)
 
-    ice_aware_curr.grid.node_lon = ice_aware_curr.grid.node_lon[:]-360
+    ice_aware_curr.grid.node_lon = ice_aware_curr.grid.node_lon[:] - 360
 #     ice_aware_curr.grid.build_celltree()
     model.movers += i_c_mover
     model.movers += i_w_mover
@@ -128,17 +130,11 @@ if __name__ == "__main__":
     scripting.make_images_dir()
     model = make_model()
     print "doing full run"
-#     rend = model.outputters[0]
-#     rend.graticule.set_DMS(True)
     startTime = datetime.now()
-    pd.profiler.enable()
+    # pd.profiler.enable()
     for step in model:
-#         if step['step_num'] == 0:
-#             rend.set_viewport(((-165, 69.25), (-162.5, 70)))
-#         if step['step_num'] == 0:
-#             rend.set_viewport(((-175, 65), (-160, 70)))
         print "step: %.4i -- memuse: %fMB" % (step['step_num'],
                                               utilities.get_mem_use())
-    print datetime.now() - startTime
-    pd.profiler.disable()
-    pd.print_stats(0.1)
+    print "it took %s to run" % (datetime.now() - startTime)
+    # pd.profiler.disable()
+    # pd.print_stats(0.1)
