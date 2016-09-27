bf0b29ffb750de20162ca66d60c90e2c32198c90
jay.hennen <jay.hennen@noaa.gov>
2016-09-27 12:47:01 -0700

    more debugging

--- py_gnome/gnome/tamoc/tamoc_spill.py
+++ py_gnome/gnome/tamoc/tamoc_spill.py
@@ -586,22 +586,24 @@ class TamocSpill(gnome.spill.spill.BaseSpill):
             end_idx = start_idx + n_LEs
             if end_idx == 0:
                 end_idx = None
-#            print '{0} to {1}'.format(start_idx, end_idx)
+            print '{0} to {1}'.format(start_idx, end_idx)
 
             data_arrays['positions'][start_idx:end_idx] = pos
             data_arrays['mass'][start_idx:end_idx] = mass_dist / n_LEs
             data_arrays['init_mass'][start_idx:end_idx] = mass_dist / n_LEs
             data_arrays['density'][start_idx:end_idx] = droplet.density
             data_arrays['droplet_diameter'][start_idx:end_idx] = droplet.radius * 2
-            total_rel += n_LEs
             v = data_arrays['rise_vel'][start_idx:end_idx]
             print len(v)
+            print data_arrays['density'][start_idx:end_idx]
+            print data_arrays['droplet_diameter'][start_idx:end_idx]
             rise_velocity_from_drop_size(v,
                                          data_arrays['density'][start_idx:end_idx],
                                          data_arrays['droplet_diameter'][start_idx:end_idx],
                                          1020, 0.000001)
             data_arrays['rise_vel'][start_idx:end_idx] = v
             print data_arrays['rise_vel'][start_idx:end_idx]
+            total_rel += n_LEs
 
         self.num_released += num_new_particles
         self.amount_released += total_mass
