b5e08d5e23f362ba681d013e442083b793129d15
James L. Makela <james.makela@noaa.gov>
2017-08-07 11:36:01 -0700

    Cleaned up the code syntax for the ship_drift_mover module. Functionality has not changed.

--- py_gnome/gnome/movers/ship_drift_mover.py
+++ py_gnome/gnome/movers/ship_drift_mover.py
@@ -1,30 +1,26 @@
 '''
 Ship drift mover
 '''
-
 import os
 import copy
-from datetime import datetime
-import math
 
-import numpy
-np = numpy
+import numpy as np
+
 from colander import (SchemaNode, String, Float, drop)
-from gnome.utilities import projections
 
 from gnome.basic_types import (velocity_rec,
                                world_point,
                                world_point_type,
                                status_code_type,
                                oil_status)
-
+from gnome.utilities import projections
 from gnome.utilities import serializable, rand
 
-from gnome import environment
 from gnome.environment import Grid
 from gnome.movers import Mover, ProcessSchema
 
 from gnome.persist.base_schema import ObjType
+
 
 class ShipDriftMoverSchema(ObjType, ProcessSchema):
     wind_file = SchemaNode(String(), missing=drop)
@@ -33,18 +29,20 @@ class ShipDriftMoverSchema(ObjType, ProcessSchema):
     grid_type = SchemaNode(Float(), missing=drop)
     drift_angle = SchemaNode(Float(), missing=drop)
 
+
 class ShipDriftMover(Mover, serializable.Serializable):
     _state = copy.deepcopy(Mover._state)
-    _state.add(update=['wind_scale','grid_type','drift_angle'], save=['wind_scale','grid_type','drift_angle'])
+    _state.add(update=['wind_scale', 'grid_type', 'drift_angle'],
+               save=['wind_scale', 'grid_type', 'drift_angle'])
     _state.add_field([serializable.Field('wind_file', save=True,
-                    read=True, isdatafile=True, test_for_eq=False),
-                    serializable.Field('topology_file', save=True,
-                    read=True, isdatafile=True, test_for_eq=False)])
+                     read=True, isdatafile=True, test_for_eq=False),
+                     serializable.Field('topology_file', save=True,
+                     read=True, isdatafile=True, test_for_eq=False)])
 
     _schema = ShipDriftMoverSchema
 
     def __init__(self, wind_file, topology_file=None, grid_type=1,
-                 drift_angle = 0, extrapolate=False, time_offset=0,
+                 drift_angle=0, extrapolate=False, time_offset=0,
                  **kwargs):
         """
         :param wind_file: file containing wind data on a grid
@@ -71,18 +69,23 @@ class ShipDriftMover(Mover, serializable.Serializable):
         # is wind_file and topology_file is stored with cy_gridwind_mover?
         self.wind_file = wind_file
         self.topology_file = topology_file
-        self.mover = Mover()
-        self.grid_type = grid_type
-        self.drift_angle = drift_angle
-        self.grid = Grid(wind_file, topology_file, grid_type)
+
         self.name = os.path.split(wind_file)[1]
-        self._wind_scale=kwargs.pop('wind_scale', 1)
+        self.drift_angle = drift_angle
+        self._wind_scale = kwargs.pop('wind_scale', 1)
+
+        self.grid_type = grid_type
+        self.grid = Grid(wind_file, topology_file, grid_type)
+
+        self.mover = Mover()
+
         super(ShipDriftMover, self).__init__(**kwargs)
 
-        #have to override any uncertainty
-        #self.grid.load_data(wind_file, topology_file)
+        # have to override any uncertainty
+        # self.grid.load_data(wind_file, topology_file)
 
         self.model_time = 0
+
         self.positions = np.zeros((0, 3), dtype=world_point_type)
         self.delta = np.zeros((0, 3), dtype=world_point_type)
         self.status_codes = np.zeros((0, 1), dtype=status_code_type)
@@ -90,7 +93,6 @@ class ShipDriftMover(Mover, serializable.Serializable):
         self.array_types.update({'windages',
                                  'windage_range',
                                  'windage_persist'})
-
 
     def __repr__(self):
         """
@@ -111,13 +113,11 @@ class ShipDriftMover(Mover, serializable.Serializable):
 
     wind_scale = property(lambda self: self._wind_scale,
                           lambda self, val: setattr(self,
-                                                    'wind_scale',
-                                                    val))
+                                                    'wind_scale', val))
 
     extrapolate = property(lambda self: self.grid.extrapolate,
                            lambda self, val: setattr(self.grid,
-                                                     'extrapolate',
-                                                     val))
+                                                     'extrapolate', val))
 
     time_offset = property(lambda self: self.grid.time_offset / 3600.,
                            lambda self, val: setattr(self.grid,
@@ -135,7 +135,6 @@ class ShipDriftMover(Mover, serializable.Serializable):
 
         self.grid.export_topology(topology_file)
 
-
     def prepare_for_model_run(self):
         """
         Override this method if a derived mover class needs to perform any
@@ -143,7 +142,6 @@ class ShipDriftMover(Mover, serializable.Serializable):
         """
         # May not need this function
         pass
-
 
     def prepare_for_model_step(self, sc, time_step, model_time_datetime):
         """
@@ -156,11 +154,11 @@ class ShipDriftMover(Mover, serializable.Serializable):
         """
         # not sure if we need to redefine this or what we want to do here
         super(ShipDriftMover, self).prepare_for_model_step(sc, time_step,
-                                                          model_time_datetime)
+                                                           model_time_datetime)
 
         # if no particles released, then no need for windage
         # TODO: revisit this since sc.num_released shouldn't be None
-        if sc.num_released is None  or sc.num_released == 0:
+        if sc.num_released is None or sc.num_released == 0:
             return
 
         self.grid.prepare_for_model_step(model_time_datetime)
@@ -190,11 +188,10 @@ class ShipDriftMover(Mover, serializable.Serializable):
             raise ValueError('The spill container does not have the required'
                              'data arrays\n' + err.message)
 
-        self.positions = \
-            self.positions.view(dtype=world_point).reshape(
-                                                    (len(self.positions),))
-        self.delta = np.zeros(len(self.positions),
-                              dtype=world_point)
+        self.positions = (self.positions.view(dtype=world_point)
+                          .reshape((len(self.positions),)))
+
+        self.delta = np.zeros(len(self.positions), dtype=world_point)
 
     def get_move(self, sc, time_step, model_time_datetime):
         """
@@ -207,15 +204,14 @@ class ShipDriftMover(Mover, serializable.Serializable):
                                     object
         """
         self.prepare_data_for_get_move(sc, model_time_datetime)
-        #will need to override get_move using grid's get_values
+
+        # will need to override get_move using grid's get_values
 
         vels = np.zeros(len(self.positions), dtype=velocity_rec)
         in_water_mask = self.status_codes == oil_status.in_water
 
         if self.active and len(self.positions) > 0:
             self.grid.get_values(self.model_time, self.positions, vels)
-            #self.grid.grid.get_values(self.model_time, self.positions, vels)
-            vel = self.grid.get_value(self.model_time, (-123.57152, 37.369436))
 
             self.delta['lat'][in_water_mask] = vels['v'] * time_step
             self.delta['long'][in_water_mask] = vels['u'] * time_step
@@ -223,7 +219,13 @@ class ShipDriftMover(Mover, serializable.Serializable):
             self.delta['lat'][in_water_mask] *= sc['windages']
             self.delta['long'][in_water_mask] *= sc['windages']
 
-            self.delta = projections.FlatEarthProjection.meters_to_lonlat(self.delta.view(dtype=np.float64).reshape(-1,3), self.positions.view(dtype=np.float64).reshape(-1,3))
+            self.delta = (projections.FlatEarthProjection
+                          .meters_to_lonlat(self.delta
+                                            .view(dtype=np.float64)
+                                            .reshape(-1, 3),
+                                            self.positions
+                                            .view(dtype=np.float64)
+                                            .reshape(-1, 3)))
 
         return (self.delta.view(dtype=world_point_type)
                 .reshape((-1, len(world_point))))
