d42583d70a7b1b46d45ded64f018afc39d6536d7
Chris Barker <Chris.Barker@noaa.gov>
2018-01-29 17:22:28 -0800

    updated conda requirements file.

--- py_gnome/gnome/__init__.py
+++ py_gnome/gnome/__init__.py
@@ -18,7 +18,7 @@ from gnomeobject import GnomeId, AddLogger
 # from gnomeobject import init_obj_log
 
 # using a PEP 404 compliant version name
-__version__ = '0.6.0'
+__version__ = '0.6.1'
 
 
 # a few imports so that the basic stuff is there
