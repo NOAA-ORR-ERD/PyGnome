9e77acb0e7a1cfdbe8a51b784d0f08b122b44719
Caitlin O'Connor <caitlin.oconnor@noaa.gov>
2018-09-14 11:31:46 -0700

    fixed overloaded function warning

--- lib_gnome/ShioTimeValue_c.cpp
+++ lib_gnome/ShioTimeValue_c.cpp
@@ -1405,7 +1405,7 @@ OSErr ShioTimeValue_c::GetInterpolatedComponent(Seconds forTime, double *value, 
 }
 
 
-OSErr ShioTimeValue_c::ReadTimeValues (char *path)
+OSErr ShioTimeValue_c::ReadShioValues (char *path)
 {
 	// code goes here, use unitsIfKnownInAdvance to tell if we're coming from a location file, 
 	// if not and it's a heights file ask if progressive or standing wave (add new field or track as 'P')
@@ -1459,7 +1459,7 @@ OSErr ShioTimeValue_c::ReadTimeValues (char *path)
 		delete _ifstream;
 	} catch(...) {
 		
-		printError("We are unable to open or read from the shio tides file. \nBreaking from ShioTimeValue_c::ReadTimeValues().");
+		printError("We are unable to open or read from the shio tides file. \nBreaking from ShioTimeValue_c::ReadShioValues().");
 		err = true;
 		goto readError;
 	}
