2c9a2d02ca69c7bfd6404d61abd2c0f60004b4b2
Chris Barker <Chris.Barker@noaa.gov>
2018-12-21 16:24:36 -0800

    start on an Oil object for GNOME

--- py_gnome/gnome/weatherers/oil.py
+++ py_gnome/gnome/weatherers/oil.py
@@ -5,50 +5,97 @@ This provides an Oil object that can be used in the GNOME weathering algorithms.
 
 """
 
-class Oil:
+from backports.functools_lru_cache import lru_cache
+
+import json
+import numpy as np
+
+
+class Oil(object):
     """
     Oil object: provides all properties and methods required by the GNOME
     weathering algorithms.
 
-    An oil has a number of properties and methods, and N psuedo components
+    An oil has a number of properties and methods, and N pseudo components
 
     For each PC, there are a number of properties, each of those properties
-    is an array with len(num_pcs)
+    is an array with length number of PCs
 
     """
-    def __init__(self, num_pcs):
+    def __init__(self,
+                 name,
+                 # Physical properties
+                 api,
+                 pour_point,
+                 solubility,  # kg/m^3
+                 # emulsification properties
+                 bullwinkle_fraction,
+                 bullwinkle_time,
+                 emulsion_water_fraction_max,
+                 densities,
+                 density_ref_temps,
+                 kvis,
+                 kvis_ref_temp,
+                 # PCs:
+                 mass_fractions,
+                 boiling_points,
+                 molecular_weights,
+                 component_densities,
+                 sara_types,
+                 ):
         """
-        create an empty oil
+        Create an oil from pseudo component data
+        """
+        self.num_pcs = len(mass_fractions)
 
-        :param num_pcs: number of psuedo components
-        """
-        self.num_pcs = num_pcs
+        # set the PC properties
+        self._set_pc_values('mass_fractions', mass_fractions)
+        self._set_pc_values('molecular_weights', molecular_weights)
+        self._set_pc_values('boiling_points', boiling_points)
+        self._set_pc_values('component_densities', component_densities)
+        if len(sara_types) == self.num_pcs:
+            self.sara_types = sara_types
+        else:
+            raise ValueError("You must have the same number of sara_types as PCs")
 
-    @property
-    def boiling_points(self):
-        """
-        the boiling points of each pseudo component
-        """
-        return self._boiling_points
+    @classmethod
+    def from_json(cls, data):
+        if type(data) in (str, unicode):
+            data = json.loads(data)
+        return cls(**num_pcs)
 
-    @property.setter
-    def boiling_points(self, bps):
-        if len(bps) != self.num_pcs:
-            raise ValueError
+
+    # def to_dict(self):
+    #     return
+
+    def _set_pc_values(self, prop, values):
+        """
+        utility that sets a property to each pseudo component
+
+        checks that it's the right size, and converts to an array
+        """
+        if len(values) != self.num_pcs:
+            raise ValueError("must be the same number of {} as there "
+                             "are pseudo components".format(prop))
+        setattr(self, prop, np.array(values, dtype=np.float64))
 
 
     @lru_cache(2)
     def vapor_pressure(self, temp, atmos_pressure=101325.0):
-        '''
+        """
+        the vapor pressure on the PCs at a given temperature
         water_temp and boiling point units are Kelvin
-        returns the vapor_pressure in SI units (Pascals)
 
-        ""
-        '''
+        :param temp: temperature in K
+
+        :returns: vapor_pressure array in SI units (Pascals)
+
+        ## Fixme: shouldn't this be in the Evaporation code?
+        """
         D_Zb = 0.97
         R_cal = 1.987  # calories
 
-        D_S = 8.75 + 1.987 * np.log(self.boiling_points)
+        D_S = 8.75 + R_cal * np.log(self.boiling_points)
         C_2i = 0.19 * self.boiling_points - 18
 
         var = 1. / (self.boiling_point - C_2i) - 1. / (temp - C_2i)
--- py_gnome/tests/unit_tests/test_weatherers/test_oil.py
+++ py_gnome/tests/unit_tests/test_weatherers/test_oil.py
@@ -2,8 +2,38 @@
 test code for the gnome oil object
 """
 
+import numpy as np
+
 from gnome.weatherers.oil import Oil
 
-def test_init():
-    oil = Oil()
+
+# one component oil
+sample_oil_1 =  {'name': 'benzene',
+                 # Physical Properties
+                 'api': 28.6,
+                 'pour_point': 278.68,
+                 'solubility': 1.78,  # kg/m^3
+                 'densities':         [886.30,   859.55,   838.73,   814.86],
+                 'density_ref_temps': [285.93,   310.93,   330.37,   352.59],
+                 'kvis':              [8.513e-7, 7.357e-7, 5.415e-7, 3.89e-7,],
+                 'kvis_ref_temp':     [283.15,   293.15,   323.15,   353.15],
+                 # emulsification properties
+                 'bullwinkle_fraction': None,
+                 'bullwinkle_time': None,
+                 'emulsion_water_fraction_max': None,
+                 # Pseudo Components:
+                 'mass_fractions': [1.0],
+                 'boiling_points': [353.05],
+                 'molecular_weights': [78.11, ],
+                 'component_densities': [814.0, ],
+                 'sara_types': ['aromatics'],
+                 }
+
+
+def test_init_1():
+    oil = Oil(**sample_oil_1)
+    assert np.alltrue(oil.boiling_points == np.array([353.05], dtype=np.float64))
+
+
+
 
