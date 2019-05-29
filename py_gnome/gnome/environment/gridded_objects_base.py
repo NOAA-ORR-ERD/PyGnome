fa68b8bb69c29b9fab0b0f46f5021eea8516f187
Caitlin O'Connor <caitlin.oconnor@noaa.gov>
2019-05-29 12:55:27 -0700

    don't change data_start and data_stop times when extrapolating

--- py_gnome/gnome/environment/gridded_objects_base.py
+++ py_gnome/gnome/environment/gridded_objects_base.py
@@ -407,14 +407,14 @@ class Variable(gridded.Variable, GnomeId):
 
     @property
     def data_start(self):
-        if self.time.min_time == self.time.max_time or self.extrapolation_is_allowed:
+        if self.time.min_time == self.time.max_time:
             return InfDateTime("-inf")
         else:
             return self.time.min_time.replace(tzinfo=None)
 
     @property
     def data_stop(self):
-        if self.time.min_time == self.time.max_time or self.extrapolation_is_allowed:
+        if self.time.min_time == self.time.max_time:
             return InfDateTime("inf")
         else:
             return self.time.min_time.replace(tzinfo=None)
@@ -537,14 +537,14 @@ class VectorVariable(gridded.VectorVariable, GnomeId):
 
     @property
     def data_start(self):
-        if self.time.min_time == self.time.max_time or self.extrapolation_is_allowed:
+        if self.time.min_time == self.time.max_time:
             return InfDateTime("-inf")
         else:
             return self.time.min_time.replace(tzinfo=None)
 
     @property
     def data_stop(self):
-        if self.time.min_time == self.time.max_time or self.extrapolation_is_allowed:
+        if self.time.min_time == self.time.max_time:
             return InfDateTime("inf")
         else:
             return self.time.max_time.replace(tzinfo=None)
