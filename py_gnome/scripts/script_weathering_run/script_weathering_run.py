e8f702dde65adb1380f76eab3f0226b886f2f22f
Chris Barker <Chris.Barker@noaa.gov>
2019-08-23 11:24:19 -0700

    a bit of cleanup of the weathering script

--- py_gnome/scripts/script_weathering_run/script_weathering_run.py
+++ py_gnome/scripts/script_weathering_run/script_weathering_run.py
@@ -4,33 +4,19 @@ Script to show how to run py_gnome with weathering
 
 This is a "just weathering" run -- no land, currents, etc.
 """
-
-from datetime import datetime, timedelta
-
 from gnome import scripting as gs
 
-# define base directory
-# base_dir = os.path.dirname(__file__)
-
-# water = Water(280.928)
-# wind = constant_wind(20., 117, 'knots')
-# waves = Waves(wind, water)
-
 def make_model():
+
     print 'initializing the model'
-
-#    start_time = datetime(2015, 5, 14, 0, 0)
-
     model = gs.Model(duration=gs.days(5))
 
 
     print 'adding outputters'
-
     model.outputters += gs.OilBudgetOutput("GNOME_oil_budget.csv")
 
     print 'adding a spill'
     # We need a spill at the very least
-
     spill = gs.point_line_release_spill(num_elements=10,  # no need for a lot of elements for a instantaneous release
                                         start_position=(0.0, 0.0, 0.0),
                                         release_time=model.start_time,
@@ -49,12 +35,11 @@ def make_model():
 
 
     model.environment += gs.Water(25, units={"temperature": "C"})
-    #waves = Waves(wind, water)
+
     waves = gs.Waves()
     model.environment += waves
 
     print 'adding the standard weatherers'
-
     model.add_weathering()
 
     return model
