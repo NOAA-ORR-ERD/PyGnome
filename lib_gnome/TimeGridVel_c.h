6fc1c7cf550f50c4a26241dac24bb796474565e0
Caitlin O'Connor <caitlin.oconnor@noaa.gov>
2019-11-05 12:12:51 -0800

    added functions to get grid and velocity data for ascii format

--- lib_gnome/TimeGridVel_c.h
+++ lib_gnome/TimeGridVel_c.h
@@ -364,6 +364,9 @@ class TimeGridCurRect_c : virtual public TimeGridVel_c
 public:
 
 	PtCurTimeDataHdl fTimeDataHdl;	
+	LongPointHdl fPtsH;
+	WORLDPOINTH fCenterPtsH;		// for curvilinear, all vertex points from file
+	GridCellInfoHdl fGridCellInfoH;
 	short fUserUnits;
 
 	TimeGridCurRect_c();
@@ -395,6 +398,13 @@ public:
 
 	virtual OSErr TextRead(std::vector<std::string> &linesInFile, std::string containingDir);
 	virtual OSErr TextRead(const char *path, const char *topFilePath);
+
+	virtual LongPointHdl		GetPointsHdl();
+	virtual WORLDPOINTH 	GetCellCenters();
+	virtual GridCellInfoHdl 	GetCellData();
+	virtual OSErr 	GetScaledVelocities(Seconds time, VelocityFRec *velocity);
+
+	virtual	bool 		IsRegularGrid(){return true;}
 };
 
 
