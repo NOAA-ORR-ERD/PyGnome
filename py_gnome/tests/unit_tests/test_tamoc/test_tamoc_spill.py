5326a54aea53299e82b71103d279aec11634fbed
Chris Barker <Chris.Barker@noaa.gov>
2020-06-03 18:05:02 -0700

    a start on a decorator for adding the Schema automatically

---
+++ py_gnome/tests/unit_tests/test_tamoc/test_tamoc_spill.py
@@ -0,0 +1,20 @@
+"""
+tests for the tamoc_spill module
+
+The TAMOC Spill module can be at least partially run without tamoc.
+
+NOTE: we might want to "mock" tamoc for this ultimately
+
+"""
+
+from gnome.tamoc.tamoc_spill import TamocSpill
+from gnome.spill.substance import NonWeatheringSubstance
+
+def test_init():
+    """
+    just making sure we can initialize it with defaults
+    """
+
+    ts = TamocSpill(substance=NonWeatheringSubstance())
+
+
---
+++ py_gnome/tests/unit_tests/test_utilities/test_schema_decorator.py
@@ -0,0 +1,35 @@
+"""
+test code for the schema decorator
+"""
+
+from gnome.persist.schema_decorator import serializable
+
+from colander import SchemaNode, String, Float, Integer, Boolean
+
+
+def test_simple():
+    @serializable
+    class Example():
+        x = 5 # not a schema object
+        y = Float()
+        z = Integer(strict=True)
+        s = String()
+        def __init__(self, x, y, z, s):
+            self.x = x
+            self.y = y
+            self.z = z
+            self.s = s
+
+
+
+    ex = Example(1, 2.0, 3, "some text")
+
+    assert ex.x == 1
+
+    assert ex._schema.__name__ == "ExampleSchema"
+
+    assert False
+
+
+
+
