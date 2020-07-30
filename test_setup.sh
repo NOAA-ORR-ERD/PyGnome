fd429013a2db14b569af99317adaaf20b34ca8b6
Naomi Wilkins <naomi.wilkin@noaa.gov>
2020-07-30 13:01:30 -0700

    added docker-compose for testing

---
+++ test_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+yum install gcc gcc-c++ netcdf-devel libXext libSM libXrender -y
+cd pygnome
+conda install --file conda_requirements_py3.txt -y
+cd py_gnome
+python ./setup.py develop
+/bin/bash
