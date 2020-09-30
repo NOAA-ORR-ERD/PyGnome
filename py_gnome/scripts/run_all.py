3dea0d9e2191e13b382e502c1130133a96d8a371
Chris Barker <Chris.Barker@noaa.gov>
2020-09-29 20:47:31 -0700

    added a bunch of stuff scripts generate to .gitignore

--- py_gnome/scripts/run_all.py
+++ py_gnome/scripts/run_all.py
@@ -14,20 +14,26 @@ import script_runner
 def run_all_with_script_runner(to_skip=[]):
     scripts = glob.glob(os.path.join(os.path.dirname(__file__),
                         'script_*/script_*.py'))
-    print scripts
-    default_skip = ['script_ny_plume/script_ny_plume.py', 'script_ny_roms/script_ny_roms.py',
-                    'script_tamoc/script_tamoc.py', 'script_tamoc/script_arctic_tamoc.py',
-                    'script_tamoc/script_gulf_tamoc.py', 'script_TAP/script_old_TAP.py']
+    print(scripts)
+
+    default_skip = ['script_ny_plume/script_ny_plume.py',
+                    'script_ny_roms/script_ny_roms.py',
+                    'script_tamoc/script_tamoc.py',
+                    'script_tamoc/script_arctic_tamoc.py',
+                    'script_tamoc/script_gulf_tamoc.py',
+                    'script_TAP/script_old_TAP.py',
+                    'script_ice/script_ice.py'
+                    ]
 
     for script in to_skip:
         default_skip = [s for s in default_skip if script not in s]
     to_skip.extend(default_skip)
     for script in to_skip:
         scripts = [s for s in scripts if script not in s]
-    print scripts
+    print(scripts)
 
     for script in scripts:
-        print 'Begin processing script: {0}'.format(script)
+        print('Begin processing script: {0}'.format(script))
 
         # clean directories first
         # script_runner.clean(os.path.dirname(script))
@@ -39,12 +45,12 @@ def run_all_with_script_runner(to_skip=[]):
         (model, imp_script) = script_runner.load_model(script, image_dir)
         script_runner.run(model)
 
-        print 'completed model run for: {0}'.format(script)
+        print('completed model run for: {0}'.format(script))
 
         if hasattr(imp_script, 'post_run'):
             imp_script.post_run(model)
 
-            print 'completed post model run for: {0}'.format(script)
+            print('completed post model run for: {0}'.format(script))
 
         # save model _state
 
@@ -67,28 +73,48 @@ def run_all_with_script_runner(to_skip=[]):
         #    print ('Exception in script_runner.run_from_save(saveloc)'
         #           '\n\t{0}'.format(ex))
 
+
 def run_all_alone():
-    # fixme -- needs to be finished...
+    """
+    Runs all the scripts, each in a subprocess
+
+    Then reports success and failures
+    """
+
     scripts = glob.glob(os.path.join(os.path.dirname(__file__),
                         'script_*/script_*.py'))
 
-    print scripts
+    # print(scripts)
 
+    # fixme: it would be good to keep track of the errors
+    successes = []
+    failures = []
     ## this could (and probably should) be made much smarter
     ##   should it use subprocess??
     for script in scripts:
-        print "**************************"
-        print "*"
-        print "*  Running:   %s"%script
-        print "*"
-        print "**************************"
-        subprocess.check_call(["python", script], shell=False)
+        print("**************************")
+        print("*")
+        print("*  Running:   %s"%script)
+        print("*")
+        print("**************************")
+        try:
+            subprocess.check_call(["python", script], shell=False)
+            successes.append(script)
+        except subprocess.CalledProcessError:
+            failures.append(script)
 
+    return successes, failures
 
 
 if __name__ == "__main__":
-    print sys.argv
+    print(sys.argv)
     to_skip = sys.argv[1:]
-    run_all_with_script_runner(to_skip)
-
+    # run_all_with_script_runner(to_skip)
+    successes, failures = run_all_alone()
+    print("Successful scripts:")
+    for s in successes:
+        print(s)
+    print("Scripts with Errors:")
+    for s in failures:
+        print(s)
 
