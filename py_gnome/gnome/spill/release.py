78a76542d2661ed5be19360b8b757589178221ae
jay.hennen <jay.hennen@noaa.gov>
2020-11-03 10:26:26 -0800

    wrong transform direction

--- py_gnome/gnome/spill/release.py
+++ py_gnome/gnome/spill/release.py
@@ -665,7 +665,7 @@ class SpatialRelease(Release):
                         pts = map(lambda pt: transform(Proj1, Proj2, pt[0], pt[1]), points)
                     else:
                         from pyproj import Transformer
-                        transformer = Transformer.from_crs("epsg:4326", "epsg:3857", always_xy=True)
+                        transformer = Transformer.from_crs("epsg:3857", "epsg:4326", always_xy=True)
                         pts = map(lambda pt: transformer.transform(pt[0],pt[1]), points)
                     poly = Polygon(pts)
                     shape_polys.append(poly)
