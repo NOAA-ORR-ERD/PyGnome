4eb371415b413735f1cadd60f0caacb2543e5856
Chris Barker <chris.barker@noaa.gov>
2021-02-17 02:01:49 +0000

    Yeardata

--- py_gnome/tests/unit_tests/test_environment/test_tide.py
+++ py_gnome/tests/unit_tests/test_environment/test_tide.py
@@ -7,6 +7,7 @@ import os
 import pytest
 from pytest import raises
 
+from gnome.environment.tide import SHIO_YEARDATA_LIMITS
 from gnome.environment import Tide
 from gnome.utilities.remote_data import get_datafile
 
@@ -15,6 +16,13 @@ from ..conftest import testdata
 
 shio_file = testdata['timeseries']['tide_shio']
 ossm_file = testdata['timeseries']['tide_ossm']
+
+
+def test_shio_data_limits():
+    """
+    make sure we can get the SHIO year data limits
+    """
+    assert SHIO_YEARDATA_LIMITS == (1980, 2025)
 
 
 def test_exceptions():
