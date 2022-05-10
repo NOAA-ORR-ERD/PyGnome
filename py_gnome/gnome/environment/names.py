c9c67c5511a2f2ea90153c183b0f820d8cdb620b
Chris Barker <Chris.Barker@noaa.gov>
2022-05-10 12:57:05 -0700

    added docstring with names -- and code to generate it.

--- py_gnome/gnome/environment/names.py
+++ py_gnome/gnome/environment/names.py
@@ -22,27 +22,116 @@ a netcdf files to one these names, PYGNOME should be able to load the file.
 **Name Mapping:**
 
 
+**grid_temperature**
+
+  Default Names: water_t,temp
+
+  CF Standard Names: sea_water_temperature,sea_surface_temperature
+
+**grid_salinity**
+
+  Default Names: salt
+
+  CF Standard Names: sea_water_salinity,sea_surface_salinity
+
+**grid_sediment**
+
+  Default Names: sand_06
+
+  CF Standard Names: 
+
+**ice_concentration**
+
+  Default Names: ice_fraction,aice
+
+  CF Standard Names: sea_ice_area_fraction
+
+**bathymetry**
+
+  Default Names: h
+
+  CF Standard Names: depth
+
+**grid_current**
+
+  Default Names: u,v,w
+
+  CF Standard Names: u,v,w
+
+**grid_wind**
+
+  Default Names: u,v
+
+  CF Standard Names: u,v
+
+**ice_velocity**
+
+  Default Names: u,v
+
+  CF Standard Names: u,v
+
 """
 
-nc_names = {'grid_temperature' : {'default_names': ['water_t', 'temp'], 'cf_names': ['sea_water_temperature', 'sea_surface_temperature']},
-            'grid_salinity' : {'default_names': ['salt'], 'cf_names': ['sea_water_salinity', 'sea_surface_salinity']},
-            'grid_sediment' : {'default_names': ['sand_06']},
-            'ice_concentration' : {'default_names': ['ice_fraction', 'aice' ], 'cf_names': ['sea_ice_area_fraction']},
-            'bathymetry' : {'default_names': ['h'], 'cf_names': ['depth']},
-            'grid_current' : {'default_names': {'u': ['u', 'U', 'water_u', 'curr_ucmp', 'u_surface', 'u_sur'],
-                                                'v': ['v', 'V', 'water_v', 'curr_vcmp', 'v_surface', 'v_sur'],
-                                                'w': ['w', 'W']},
-                              'cf_names': {'u': ['eastward_sea_water_velocity', 'surface_eastward_sea_water_velocity'],
-                                           'v': ['northward_sea_water_velocity', 'surface_northward_sea_water_velocity'],
-                                           'w': ['upward_sea_water_velocity']}},
-            'grid_wind' : {'default_names': {'u': ['air_u', 'Air_U', 'air_ucmp', 'wind_u'],
-                                             'v': ['air_v', 'Air_V', 'air_vcmp', 'wind_v']},
-                           'cf_names': {'u': ['eastward_wind', 'eastward wind'],
-                                        'v': ['northward_wind', 'northward wind']}},
-            'ice_velocity' : {'default_names': {'u': ['ice_u','uice'], 'v': ['ice_v','vice']},
-                              'cf_names': {'u': ['eastward_sea_ice_velocity'],
-                                           'v': ['northward_sea_ice_velocity']}},
-            }
+nc_names = {
+    'grid_temperature': {
+        'default_names': ['water_t', 'temp'],
+        'cf_names': ['sea_water_temperature', 'sea_surface_temperature']
+    },
+    'grid_salinity': {
+        'default_names': ['salt'],
+        'cf_names': ['sea_water_salinity', 'sea_surface_salinity']
+    },
+    'grid_sediment': {
+        'default_names': ['sand_06'],
+        'cf_names': []
+    },
+    'ice_concentration': {
+        'default_names': ['ice_fraction', 'aice'],
+        'cf_names': ['sea_ice_area_fraction']
+    },
+    'bathymetry': {
+        'default_names': ['h'],
+        'cf_names': ['depth']
+    },
+    'grid_current': {
+        'default_names': {
+            'u': ['u', 'U', 'water_u', 'curr_ucmp', 'u_surface', 'u_sur'],
+            'v': ['v', 'V', 'water_v', 'curr_vcmp', 'v_surface', 'v_sur'],
+            'w': ['w', 'W']
+        },
+        'cf_names': {
+            'u': [
+                'eastward_sea_water_velocity',
+                'surface_eastward_sea_water_velocity'
+            ],
+            'v': [
+                'northward_sea_water_velocity',
+                'surface_northward_sea_water_velocity'
+            ],
+            'w': ['upward_sea_water_velocity']
+        }
+    },
+    'grid_wind': {
+        'default_names': {
+            'u': ['air_u', 'Air_U', 'air_ucmp', 'wind_u'],
+            'v': ['air_v', 'Air_V', 'air_vcmp', 'wind_v']
+        },
+        'cf_names': {
+            'u': ['eastward_wind', 'eastward wind'],
+            'v': ['northward_wind', 'northward wind']
+        }
+    },
+    'ice_velocity': {
+        'default_names': {
+            'u': ['ice_u', 'uice'],
+            'v': ['ice_v', 'vice']
+        },
+        'cf_names': {
+            'u': ['eastward_sea_ice_velocity'],
+            'v': ['northward_sea_ice_velocity']
+        }
+    },
+}
 
 def build_table_for_docstring():
     """
@@ -82,22 +171,30 @@ def insert_names_table(table_text):
         for line in contents:
             outfile.write(line)
 
+    shutil.copy(tempfile, this)
+    os.remove(tempfile)
+
+
+def build_names_table():
+    table = []
+    for env_obj, names in nc_names.items():
+        table.append(f"\n**{env_obj:}**\n")
+        table.append(f"\n  Default Names: {','.join(names['default_names'])}\n")
+        table.append(f"\n  CF Standard Names: {','.join(names['cf_names'])}\n")
+    return ''.join(table)
+
 
 if __name__ == "__main__":
 
+    import os
     import sys
+    import shutil
 
     if "rebuild" in sys.argv:
         print("rebuilding docstring")
         #table_text = build_table_for_docstring()
 
-        table_text = """
-        Some sample text
-        to test with
-
-        """
-
-        insert_names_table(table_text)
+        insert_names_table(build_names_table())
     else:
         print("Doing Nothing")
         print('To rebuild the docstring, pass "rebuild" in on the command line:')
