cb70b9a24e2437497ec0363cfef9fff38313368a
Amy MacFadyen <amy.macfadyen@noaa.gov>
2023-01-09 14:11:56 -0800

    Skipping component mover test on Windows (fatal crash occurs)

--- py_gnome/tests/unit_tests/test_movers/test_component_mover.py
+++ py_gnome/tests/unit_tests/test_movers/test_component_mover.py
@@ -146,7 +146,7 @@ def test_get_scaled_velocities_two_patterns():
     assert vels['v'][10] == -0.09848350841987873
 
 
-
+@pytest.mark.skipif(sys.platform=="win32", reason = "not working on windows - fatal crash")
 def test_get_scaled_velocities_one_pattern():
     component = ComponentMover(filename1=curr1_file,
                                wind=wnd,
