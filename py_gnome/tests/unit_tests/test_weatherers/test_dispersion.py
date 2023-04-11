86eab81bced4fee5a4954b08597367dbd66799f1
Leo Geng <leo.geng@noaa.gov>
2023-04-11 17:03:34 +0000

    Dispersion

--- py_gnome/tests/unit_tests/test_weatherers/test_dispersion.py
+++ py_gnome/tests/unit_tests/test_weatherers/test_dispersion.py
@@ -1,10 +1,6 @@
 '''
 Test natural dispersion module
 '''
-
-
-
-
 from datetime import timedelta
 
 import pytest
@@ -101,18 +97,16 @@ def test_dispersion_not_active(oil, temp, num_elems):
     assert np.all(sc.mass_balance['sedimentation'] == 0)
 
 
-@pytest.mark.xfail
+#@pytest.mark.xfail
 # the test oils don't match the data base, using so tests don't depend on db
+# Note: test values appear to be reasonable as Feb-13-2023
+# Note: this test may change if any other weathering algorithms change
 @pytest.mark.parametrize(('oil', 'temp', 'dispersed'),
-                         [('oil_bahia', 288.7, 133.784),
-                         #('ABU SAFAH', 288.7, 63.076),
-                          # ('ALASKA NORTH SLOPE (MIDDLE PIPELINE)',
-                          ('oil_ans_mp',
-                           288.7, 592.887),
-                          # ('BAHIA', 288.7, 14.472)
+                         [('oil_bahia', 288.7, 264.076),
+                          ('oil_ans_mp', 288.7, 404.244),
                           ]
                          )
-def test_full_run(sample_model_fcn2, oil, temp, dispersed):
+def test_full_run_DS1988(sample_model_fcn2, oil, temp, dispersed):
     '''
     test dispersion outputs post step for a full run of model. Dump json
     for 'weathering_model.json' in dump directory
@@ -140,8 +134,45 @@ def test_full_run(sample_model_fcn2, oil, temp, dispersed):
                 assert (sc.mass_balance['sedimentation'] > 0)
 
     sc = list(model.spills.items())[0]
-    print((sc.mass_balance['natural_dispersion'], dispersed))
 
+    assert np.isclose(sc.mass_balance['natural_dispersion'], dispersed,
+                      atol=0.001)
+
+@pytest.mark.parametrize(('oil', 'temp', 'dispersed'),
+                         [('oil_bahia', 288.7, 4131.36),
+                          ('oil_ans_mp', 288.7, 8158.10),
+                          ]
+                         )
+def test_full_run_Li2017(sample_model_fcn2, oil, temp, dispersed):
+    '''
+    test dispersion outputs post step for a full run of model. Dump json
+    for 'weathering_model.json' in dump directory
+    '''
+    model = sample_model_weathering2(sample_model_fcn2, oil, temp)
+    model.environment += [Water(temp), wind, waves]
+    model.weatherers += Evaporation()
+    model.weatherers += Emulsification(waves)
+    model.weatherers += NaturalDispersion(algorithm='Li2017')
+
+    # set make_default_refs to True for objects contained in model after adding
+    # objects to the model
+    model.set_make_default_refs(True)
+
+    for step in model:
+        for sc in list(model.spills.items()):
+            if step['step_num'] > 0:
+                # print ("Dispersed: {0}".
+                #        format(sc.mass_balance['natural_dispersion']))
+                # print ("Sedimentation: {0}".
+                #        format(sc.mass_balance['sedimentation']))
+                # print "Completed step: {0}\n".format(step['step_num'])
+
+                assert (sc.mass_balance['natural_dispersion'] > 0)
+                assert (sc.mass_balance['sedimentation'] > 0)
+
+    sc = list(model.spills.items())[0]
+    print('lllleeeeoooo', sc.mass_balance['natural_dispersion'])
+    print('000000000000', dispersed)
     assert np.isclose(sc.mass_balance['natural_dispersion'], dispersed,
                       atol=0.001)
 
@@ -163,18 +194,17 @@ def test_full_run_disp_not_active(sample_model_fcn):
         # print ("Completed step: {0}"
         #        .format(step['step_num']))
 
-@pytest.mark.skipif(reason="serialization for weatherers overall needs review")
+#@pytest.mark.skipif(reason="serialization for weatherers overall needs review")
 def test_serialize_deseriailize():
     'test serialize/deserialize for webapi'
     wind = constant_wind(15., 0)
     waves = Waves(wind, Water())
-    e = NaturalDispersion(waves)
+    e = NaturalDispersion(waves, algorithm='Li2017')
     json_ = e.serialize()
-    json_['waves'] = waves.serialize()
 
     # deserialize and ensure the dict's are correct
     d_ = NaturalDispersion.deserialize(json_)
-    assert d_['waves'] == Waves.deserialize(json_['waves'])
-    d_['waves'] = waves
-    e.update_from_dict(d_)
-    assert e.waves is waves
+    assert d_.waves == Waves.deserialize(json_['waves'])
+    assert d_.waves == waves
+    assert d_.algorithm == 'Li2017'
+
