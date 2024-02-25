d162847f9a628cf876f427a1e28aba5d344bf46e
James.Makela <james.makela@noaa.gov>
2024-02-25 00:05:48 -0800

    Prepping the cmakelists.txt file to start compiling the point_in_polygons extension.

--- py_gnome/gnome/utilities/geometry/setup.py
+++ py_gnome/gnome/utilities/geometry/setup.py
@@ -3,16 +3,17 @@
 """
 setup.py for geometry package
 
-It's now built with the main gnome setup.py, but kept this here for easier testing in place...
-only useful now for "develop" mode
+It's now built with the main gnome setup.py, but kept this here for
+easier testing in place...
+Only useful now for "develop" mode
 """
 
-#from distutils.core import setup
-from setuptools import setup # to support "develop" mode
+# from distutils.core import setup
+from setuptools import setup  # to support "develop" mode
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-import numpy # for the includes for the Cython code
+import numpy  # for the includes for the Cython code
 
 
 ext_modules = [Extension("cy_point_in_polygon",
@@ -22,19 +23,19 @@ ext_modules = [Extension("cy_point_in_polygon",
                ]
 
 setup(
-    name = "geometry",
-    version = "0.03",
-    author = "Chris Barker",
-    author_email = "Chris.Barker@noaa.gov",
-    description = ("some geometry for GNOME"),
-#    long_description=read('README'),
+    name="geometry",
+    version="0.03",
+    author="Chris Barker",
+    author_email="Chris.Barker@noaa.gov",
+    description=("some geometry for GNOME"),
+    # long_description=read('README'),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
         ],
-    cmdclass = {'build_ext': build_ext},
-#    packages = ["geometry"],
-    ext_modules = ext_modules,
-#    modules = ["BBox.py",]
-    scripts = [],
+    cmdclass={'build_ext': build_ext},
+    # packages=["geometry"],
+    ext_modules=ext_modules,
+    # modules=["BBox.py",]
+    scripts=[],
     )
