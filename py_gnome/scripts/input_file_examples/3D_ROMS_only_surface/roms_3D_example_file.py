85a68a45a5ca0c41790967d7d45167a7acf04d52
Chris Barker <chris.barker@noaa.gov>
2025-05-13 19:05:00 +0000

    Added example files dir for gridded data examples.

---
+++ py_gnome/scripts/input_file_examples/3D_ROMS_only_surface/roms_3D_example_file.py
@@ -0,0 +1,52 @@
+"""
+Example of a ROMS 3D file with depth info that PYGNOME doesn't understand
+
+But it works in surface only mode.
+"""
+
+from pathlib import Path
+import gnome.scripting as gs
+
+HERE = Path(__file__).parent
+
+# download the example file, if it's not already there
+cur_file = gs.get_datafile(HERE / '3D_ROMS_example.nc', 'gridded_test_files')
+
+curr = gs.GridCurrent.from_netCDF(HERE / cur_file)
+
+# some grid info
+min_lat = curr.grid.node_lat.min()
+min_lon = curr.grid.node_lon.min()
+max_lat = curr.grid.node_lat.max()
+max_lon = curr.grid.node_lon.max()
+
+
+bounds = ((min_lon, min_lat), (max_lon, max_lat))
+print("bounds: ", bounds)
+
+spill_location = (-157.9, 21.2)
+
+# make sure it runs in gnome
+model = gs.Model(start_time=curr.data_start,
+                 duration=gs.days(2),
+                 # duration=curr.data_stop - curr.data_start,
+                 )
+model.map = gs.MapFromBNA(HERE / "oahu_coast.bna")
+model.movers += gs.CurrentMover(curr)
+model.movers += gs.RandomMover()
+model.spills += gs.point_line_spill(num_elements=100,
+                                    start_position=spill_location,
+                                    release_time=model.start_time
+                                    )
+
+model.outputters += gs.Renderer(
+    map_filename=model.map,
+    output_timestep=gs.hours(1),
+    output_dir=HERE,
+    image_size=(800, 600),
+    projection=None,
+    #viewport=bounds,
+    formats=['gif'],
+    )
+
+model.full_run()
---
+++ py_gnome/scripts/input_file_examples/run_all.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+'''
+Script to run all the input file examples
+
+python run_all.py
+    run all the scripts, except the ones with known issues.
+
+python run_all.py no_skip
+    run all the scripts, skipping none
+
+python run_all.py script_one script_two
+    run the specified scripts.
+'''
+import sys
+import datetime
+from pathlib import Path
+import subprocess
+
+
+def load_known_failures():
+    known_fails = []
+    with open('known_broken_scripts.txt') as infile:
+        for line in infile:
+            line = line.strip().split('#')[0]
+            if not line:
+                continue
+            known_fails.append(Path(line))
+    return known_fails
+
+
+def run_all(to_skip=[]):
+    """
+    Runs all the scripts, each in a subprocess
+
+    Then reports success and failures to stdout, as well as to:
+
+    script_results.txt
+
+    :param to_skip: list of scripts to skip -- useful for known failures
+
+    script returns number of failures as error code, so it can be used
+    in a CI test run. (e.g. 0 error code means zero failed scripts)
+    """
+    scripts = {p.relative_to(Path.cwd())
+               for p in Path(__file__).parent.glob('*/*_example_file.py')}
+
+    print(f"{len(scripts)} total scripts")
+    scripts = scripts.difference(to_skip)
+    print(f"{len(scripts)} total unskipped scripts")
+
+
+    for p in to_skip:
+        print(f"Skipping: {repr(p)}")
+
+    for p in scripts:
+        print(f"Running: {repr(p)}")
+
+    successes = []
+    failures = []
+
+    for script in scripts:
+        print("**************************")
+        print("*")
+        print("*  Running:   %s"%script)
+        print("*")
+        print("**************************")
+
+        try:
+            subprocess.check_call(["python", script], shell=False)
+            successes.append(script)
+        except subprocess.CalledProcessError:
+            failures.append(script)
+
+    with open("script_results.txt", 'w', encoding='utf-8') as outfile:
+        outfile.write("PyGNOME Script Runner Report\n")
+        outfile.write(f"Produced: {datetime.datetime.now()}\n")
+
+        outfile.write("\nScripts that ran without Errors:\n")
+        outfile.write("--------------------------------\n\n")
+        outfile.writelines(f"{script}\n" for script in successes)
+
+        outfile.write("\nScripts that Errored out:\n")
+        outfile.write("-------------------------\n\n")
+        outfile.writelines(f"{script}\n" for script in failures)
+
+    return successes, failures
+
+
+if __name__ == "__main__":
+    print(sys.argv)
+
+    try:
+        sys.argv.remove("no_skip")
+        no_skip = True
+    except ValueError:
+        no_skip = False
+
+    to_skip = [Path(p) for p in sys.argv[1:]]
+
+    if not (to_skip or no_skip):
+        to_skip = load_known_failures()
+
+    successes, failures = run_all(to_skip)
+    print("Successful scripts:")
+
+    for s in successes:
+        print(s)
+
+    print("Scripts with Errors:")
+
+    for s in failures:
+        print(s)
+
+    sys.exit(len(failures))
