084d8b31cc680b4f0816882f3c0cbec978139b28
Chris Barker <Chris.Barker@noaa.gov>
2025-08-13 13:01:26 -0700

    skipping tamoc tests if tamoc is not installed.

--- py_gnome/tests/unit_tests/test_tamoc/test_tamoc_spill.py
+++ py_gnome/tests/unit_tests/test_tamoc/test_tamoc_spill.py
@@ -5,10 +5,22 @@ The TAMOC Spill module can be at least partially run without tamoc.
 
 NOTE: we might want to "mock" tamoc for this ultimately
 
+but there's hardly anything here anyway...
+
 """
 
-from gnome.tamoc.tamoc_spill import TamocSpill
 from gnome.spills.substance import NonWeatheringSubstance
+import pytest
+
+try:
+    # we don't actually need it directly in the tests -- but need to know if can be imported.
+    import tamoc as tamoc_raw
+    from gnome.tamoc.tamoc_spill import TamocSpill
+except ImportError:
+    # if we can't import the tamoc module all tests in this module are skipped.
+    pytestmark = pytest.mark.skipif(True, reason="this test requires the tamoc package")
+
+
 
 def test_init():
     """
