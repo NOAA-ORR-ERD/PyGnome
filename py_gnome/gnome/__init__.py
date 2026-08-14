229bff7942e97b1c76c152ee01ee742ed00858d5
Jay Hennen <jay.hennen@noaa.gov>
2026-08-14 10:33:21 -0700

    removed pixi lock file, updated init version check

--- py_gnome/gnome/__init__.py
+++ py_gnome/gnome/__init__.py
@@ -72,7 +72,7 @@ def check_dependency_versions():
         else:
             return True
 
-    libs = [('gridded', '0.7.5', ''),
+    libs = [('gridded', '0.8.0', ''),
             ('nucos', '3.4.1', ''),
             ('py_gd', '2.3.3', ''),
             ('adios_db', '1.2.7', 'Only required to use the ADIOS Database '
